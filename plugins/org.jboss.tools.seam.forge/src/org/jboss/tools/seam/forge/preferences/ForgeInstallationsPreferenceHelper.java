23a9fd3e2a3299fe4babf6723f31dd1079a722eb
Koen Aers <koen.aers@jboss.com>
2011-03-10 23:07:45 +0000

    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@29685 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ plugins/org.jboss.tools.seam.forge/src/org/jboss/tools/seam/forge/preferences/ForgeInstallationsPreferenceHelper.java
@@ -0,0 +1,19 @@
+package org.jboss.tools.seam.forge.preferences;
+
+
+public class ForgeInstallationsPreferenceHelper {
+
+	public static void removeVMs() {
+		// TODO Auto-generated method stub		
+	}
+
+	public static void addVM() {
+		// TODO Auto-generated method stub		
+	}
+
+	public static void editVM() {
+		// TODO Auto-generated method stub		
+	}
+
+
+}
