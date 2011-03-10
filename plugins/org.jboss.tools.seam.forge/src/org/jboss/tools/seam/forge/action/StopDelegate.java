eb7618991d3749eb58f3b09c9ba399a463ac5a6f
Koen Aers <koen.aers@jboss.com>
2011-03-10 23:06:37 +0000

    even more runtime preferences stuf - page is working - initialization is working
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@29682 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.seam.forge/src/org/jboss/tools/seam/forge/action/StopDelegate.java
+++ plugins/org.jboss.tools.seam.forge/src/org/jboss/tools/seam/forge/action/StopDelegate.java
@@ -4,13 +4,13 @@ import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IViewActionDelegate;
 import org.eclipse.ui.IViewPart;
-import org.jboss.tools.seam.forge.runtime.Manager;
+import org.jboss.tools.seam.forge.launching.ForgeRuntime;
 
 public class StopDelegate implements IViewActionDelegate {
 	
 	@Override
 	public void run(IAction action) {
-		Manager.INSTANCE.stopForge();
+		ForgeRuntime.INSTANCE.stopForge();
 	}
 
 	@Override
