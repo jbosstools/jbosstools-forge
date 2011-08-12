079384b349635558eb41195a3a65ffe33b270ea9
Koen Aers <koen.aers@jboss.com>
2011-08-12 15:08:07 +0000

    cleanup
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@33839 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ plugins/org.jboss.tools.forge.runtime.ext/src/org/jboss/tools/forge/runtime/ext/EventHandler.java
@@ -0,0 +1,24 @@
+package org.jboss.tools.forge.runtime.ext;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import org.jboss.forge.shell.Shell;
+import org.jboss.forge.shell.events.PostStartup;
+
+public class EventHandler {
+	
+	private static final String ESCAPE = new String(new char[] { 27, '[', '%'} );
+	
+	@Inject
+	private Shell shell;
+	
+	public void startup(@Observes PostStartup event) {
+		sendEscaped("PostStartup");
+	}
+	
+	private void sendEscaped(String str) {
+		shell.print(ESCAPE + str + ESCAPE); 
+	}
+
+}
