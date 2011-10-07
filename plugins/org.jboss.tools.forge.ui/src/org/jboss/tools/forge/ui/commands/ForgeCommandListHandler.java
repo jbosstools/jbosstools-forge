f63741d6bc9b29dd354615aac1736e5a56234d61
Koen Aers <koen.aers@jboss.com>
2011-10-07 17:42:47 +0000

    JBIDE-9660: the Forge runtime can now be started by the Cmd+4 shortcut
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@35471 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/commands/ForgeCommandListHandler.java
+++ plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/commands/ForgeCommandListHandler.java
@@ -2,34 +2,80 @@ package org.jboss.tools.forge.ui.commands;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.jboss.tools.forge.core.process.ForgeRuntime;
+import org.jboss.tools.forge.ui.ForgeUIPlugin;
 import org.jboss.tools.forge.ui.dialog.ForgeCommandListDialog;
 import org.jboss.tools.forge.ui.part.ForgeView;
 
 public class ForgeCommandListHandler extends AbstractHandler {
 
 	public Object execute(ExecutionEvent executionEvent) {
-
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(executionEvent);
 		if (window == null) {
 			return null;
+		}				
+		ForgeRuntime runtime = getForgeRuntime(window);
+		if (isStarting(runtime)) {
+			showWaitUntilStartedMessage();
+		} else if (!(isRunning(runtime))) {
+			askUserToStartRuntime(window); 
 		}
+		if (runtime != null && ForgeRuntime.STATE_RUNNING.equals(runtime.getState())) {
+			new ForgeCommandListDialog(window, runtime).open();
+		}
+		return null;		
+	}
+	
+	private boolean isStarting(ForgeRuntime runtime) {
+		return runtime != null && ForgeRuntime.STATE_STARTING.equals(runtime.getState());
+	}
+	
+	private boolean isRunning(ForgeRuntime runtime) {
+		return runtime != null && ForgeRuntime.STATE_RUNNING.equals(runtime.getState());
+	}
+	
+	private ForgeRuntime getForgeRuntime(IWorkbenchWindow window) {
 		IViewPart part = window.getActivePage().findView(ForgeView.ID);
-		if (part == null || !(part instanceof ForgeView)) {
+		if (part != null && part instanceof ForgeView) {
+			return ((ForgeView)part).getRuntime();
+		} else {
 			return null;
 		}
-		
-		ForgeRuntime runtime = ((ForgeView)part).getRuntime();
-		if (runtime == null || !(ForgeRuntime.STATE_RUNNING.equals(runtime.getState()))) {
-			return null;
+	}
+	
+	private void showWaitUntilStartedMessage() {
+		MessageDialog.openInformation(null, "Forge Starting", "Forge is starting. Please wait until the Forge runtime is started");
+	}
+	
+	private void askUserToStartRuntime(final IWorkbenchWindow window) {
+		boolean start = MessageDialog.open(
+				MessageDialog.QUESTION, 
+				null, 
+				"Forge Not Running", 
+				"Forge is not running. Do you want to start the Forge runtime?", 
+				SWT.NONE);
+		if (start) {
+			Display.getCurrent().asyncExec(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						IViewPart part = window.getActivePage().showView(ForgeView.ID);
+						if (part != null && part instanceof ForgeView) {
+							((ForgeView)part).startForge();
+						}	 
+					} catch (PartInitException e) {
+						ForgeUIPlugin.log(e);
+					}						
+				}				
+			});
 		}
-		
-		new ForgeCommandListDialog(window, runtime).open();
-		
-		return null;
 	}
 
 }
