7bb97ae54405ec60ba78b89e0fdead140d57ff6a
Koen Aers <koen.aers@jboss.com>
2011-11-28 18:45:41 +0000

    JBIDE-10203: Open Command Should Show File in Editor or Expand Folder in Explorer
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@36710 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/console/OpenPostProcessor.java
+++ plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/console/OpenPostProcessor.java
@@ -1,18 +1,24 @@
 package org.jboss.tools.forge.ui.console;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TreePath;
 import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.rse.ui.view.IRSEViewPart;
+import org.eclipse.rse.ui.view.ISystemViewElementAdapter;
+import org.eclipse.rse.ui.view.SystemAdapterHelpers;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
@@ -24,13 +30,57 @@ import org.jboss.tools.forge.ui.ForgeUIPlugin;
 public class OpenPostProcessor {
 	
 	public void postProcess(String line, String currentResourcePath) {
-		String resourceToOpen = currentResourcePath + File.separator + line.substring(5).trim();
+		String resourceToOpen = line.substring(5).trim();
+		if (!resourceToOpen.startsWith("/")) {
+			resourceToOpen = currentResourcePath + File.separator + resourceToOpen;
+		}
 		IPath path = new Path(resourceToOpen);
 		IFileStore fileStore = EFS.getLocalFileSystem().getStore(new Path(resourceToOpen));
 		if (fileStore.fetchInfo().isDirectory()) {
-			expandDirectoryInExplorer(path);
+			IContainer container = ResourcesPlugin.getWorkspace().getRoot().getContainerForLocation(path);
+			if (container != null) {
+				expandWorkspaceDirectory(container);
+			} else {
+				expandSystemDirectory(fileStore);
+			}
 		} else {
 			openFileInEditor(fileStore);
+		}
+	}
+	
+	private void expandSystemDirectory(IFileStore fileStore) {
+		IWorkbenchPage workbenchPage = ForgeCommandPostProcessorHelper.getActiveWorkbenchPage();
+		IViewPart remoteSystemView = workbenchPage.findView("org.eclipse.rse.ui.view.systemView");
+		if (remoteSystemView == null) {
+			try {
+				remoteSystemView = workbenchPage.showView("org.eclipse.rse.ui.view.systemView");
+			} catch (PartInitException e) {
+				ForgeUIPlugin.log(e);
+			}
+		}
+		if (remoteSystemView != null) {
+			expandInRemoteSystemView(remoteSystemView, fileStore);
+		}
+	}
+	
+	private void expandWorkspaceDirectory(IContainer container) {
+		IWorkbenchPage workbenchPage = ForgeCommandPostProcessorHelper.getActiveWorkbenchPage();
+		if (workbenchPage != null) {
+			IViewPart projectExplorer = workbenchPage.findView("org.eclipse.ui.navigator.ProjectExplorer");
+			if (projectExplorer != null && projectExplorer instanceof CommonNavigator) {
+				expandInProjectExplorer((CommonNavigator)projectExplorer, container);
+			} 
+			IViewPart packageExplorer = workbenchPage.findView("org.eclipse.jdt.ui.PackageExplorer"); 
+			if (packageExplorer == null && projectExplorer == null) {
+				try {
+					packageExplorer = workbenchPage.showView("org.eclipse.jdt.ui.PackageExplorer");
+				} catch (PartInitException e) {
+					ForgeUIPlugin.log(e);
+				}
+			} 
+			if (packageExplorer != null) {
+				expandInPackageExplorer(packageExplorer, container);
+			}
 		}
 	}
 	
@@ -42,28 +92,6 @@ public class OpenPostProcessor {
 			}
 		} catch (PartInitException e) {
 			ForgeUIPlugin.log(e);
-		}
-	}
-	
-	private void expandDirectoryInExplorer(IPath path) {
-		IWorkbenchPage workbenchPage = ForgeCommandPostProcessorHelper.getActiveWorkbenchPage();
-		IContainer objectToSelect = ResourcesPlugin.getWorkspace().getRoot().getContainerForLocation(path);
-		if (objectToSelect != null && workbenchPage != null) {
-			IViewPart projectExplorer = workbenchPage.findView("org.eclipse.ui.navigator.ProjectExplorer");
-			if (projectExplorer != null && projectExplorer instanceof CommonNavigator) {
-				expandInProjectExplorer((CommonNavigator)projectExplorer, objectToSelect);
-			} 
-			IViewPart packageExplorer = workbenchPage.findView("org.eclipse.jdt.ui.PackageExplorer"); 
-			if (packageExplorer == null && projectExplorer == null) {
-				try {
-					packageExplorer = workbenchPage.showView("org.eclipse.jdt.ui.PackageExplorer");
-				} catch (PartInitException e) {
-					ForgeUIPlugin.log(e);
-				}
-			} 
-			if (packageExplorer != null) {
-				expandInPackageExplorer(packageExplorer, objectToSelect);
-			}
 		}
 	}
 	
@@ -80,6 +108,60 @@ public class OpenPostProcessor {
 		Object treeViewer = packageExplorer.getAdapter(ISelectionProvider.class);
 		if (treeViewer != null && treeViewer instanceof TreeViewer) {
 			((TreeViewer)treeViewer).expandToLevel(JavaCore.create(container), 1);
+		}
+	}
+	
+	private Viewer getViewer(IViewPart remoteSystemView) {
+		if (remoteSystemView instanceof IRSEViewPart) {
+			return ((IRSEViewPart)remoteSystemView).getRSEViewer();
+		} else {
+			return null;
+		}
+	}
+	
+	private ArrayList<String> createSegmentNames(IFileStore fileStore) {
+		ArrayList<String> result = new ArrayList<String>();
+		while (fileStore.getParent() != null) {
+			result.add(0, fileStore.getName());
+			fileStore = fileStore.getParent();
+		}
+		result.add(0, "/");
+		result.add(0, "Root");
+		result.add(0, "Local Files");
+		result.add(0, "Local");
+		return result;
+	}
+	
+	private void expandInRemoteSystemView(
+			IViewPart remoteSystemView, 
+			IFileStore fileStore) {
+		Viewer viewer = getViewer(remoteSystemView);
+		Object input = viewer.getInput();
+		ArrayList<String> names = createSegmentNames(fileStore);
+		ArrayList<Object> treeSegments = new ArrayList<Object>();
+		for (String name : names) {
+			if (input != null && input instanceof IAdaptable) {
+				ISystemViewElementAdapter adapter = SystemAdapterHelpers.getViewAdapter(input);
+				for (Object object : adapter.getChildren((IAdaptable)input, null)) {
+					if (object instanceof IAdaptable) {
+						adapter = SystemAdapterHelpers.getViewAdapter(object);
+						if (name.equals(adapter.getText(object))) {
+							input = object;
+							treeSegments.add(input);
+							break;
+						}
+					}
+				}
+			} else {
+				treeSegments.clear();
+				break;
+			}
+		}
+		TreePath treePath = new TreePath(treeSegments.toArray());
+		viewer.setSelection(new StructuredSelection(treePath));
+		if (viewer instanceof TreeViewer) {
+			((TreeViewer)viewer).expandToLevel(treePath, 1);
+			
 		}
 	}
 	
