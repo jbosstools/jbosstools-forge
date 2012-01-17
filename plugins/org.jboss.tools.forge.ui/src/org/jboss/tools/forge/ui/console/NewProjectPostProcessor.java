04fe0cdb0c468f7b1ce57c066e331deedc259012
Koen Aers <koen.aers@jboss.com>
2012-01-17 08:16:44 +0000

    JBIDE-8911: Forge should not swallow errors
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@37888 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/console/NewProjectPostProcessor.java
+++ plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/console/NewProjectPostProcessor.java
@@ -1,8 +1,16 @@
 package org.jboss.tools.forge.ui.console;
 
+import java.io.File;
 import java.util.Map;
 
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.jboss.tools.forge.core.process.ForgeRuntime;
 import org.jboss.tools.forge.importer.ProjectImporter;
+import org.jboss.tools.forge.ui.part.ForgeView;
+import org.jboss.tools.forge.ui.util.ForgeHelper;
 
 
 public class NewProjectPostProcessor implements ForgeCommandPostProcessor {
@@ -10,11 +18,56 @@ public class NewProjectPostProcessor implements ForgeCommandPostProcessor {
 	@Override
 	public void postProcess(Map<String, String> commandDetails) {
 		String projectPath = commandDetails.get("cpn");
-		int index = projectPath.lastIndexOf('/');
-		String projectDirName = projectPath.substring(index + 1);
-		String projectBaseDirPath = projectPath.substring(0, index);
-		ProjectImporter importer = new ProjectImporter(projectBaseDirPath, projectDirName);
-		importer.importProject();
+		IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
+		String workspacePath = workspaceRoot.getLocation().toOSString();
+		if (workspacePath.equals(projectPath)) {
+			if (MessageDialog.open(
+					MessageDialog.QUESTION, 
+					null, 
+					"Project Import Failed", 
+					"The Forge runtime created the project in the workspace root. " +
+					"Such a project cannot be imported.\n" +
+					"Do you want to remove the created artifacts?",
+					SWT.NONE)) {
+				String fileSeparator = System.getProperty("file.separator");
+				String pomPath = projectPath + fileSeparator + "pom.xml";
+				File pomFile = new File(pomPath);
+				if (pomFile.exists()) {
+					delete(pomFile);
+				}
+				String srcPath = projectPath + fileSeparator + "src";
+				File srcDir = new File(srcPath);
+				if (srcDir.exists()) {
+					delete(srcDir);
+				}
+				resetRuntime();
+			}
+		} else {
+			int index = projectPath.lastIndexOf('/');
+			String projectDirName = projectPath.substring(index + 1);
+			String projectBaseDirPath = projectPath.substring(0, index);
+			ProjectImporter importer = new ProjectImporter(projectBaseDirPath, projectDirName);
+			importer.importProject();
+		}
+	}
+	
+	private void delete(File f) {
+		if (f.isDirectory()) {
+			for (String s : f.list()) {
+				delete(new File(f, s));
+			}
+		}
+		f.delete();
+	}
+	
+	private void resetRuntime() {
+		ForgeView forgeView = ForgeHelper.getForgeView();
+		if (forgeView != null) {
+			ForgeRuntime runtime = forgeView.getRuntime();
+			if (runtime != null && ForgeRuntime.STATE_RUNNING.equals(runtime.getState())) {
+				runtime.sendInput("reset\n");
+			}
+		}
 	}
 
 }
