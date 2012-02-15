c27d379c62853f56e30050e86bf4046844057540
Koen Aers <koen.aers@jboss.com>
2012-02-15 10:13:30 +0000

    JBIDE-10904: Open command does not work when including ~ symbol for home folder in path to open
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38742 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/console/OpenPostProcessor.java
+++ plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/console/OpenPostProcessor.java
@@ -7,7 +7,7 @@ public class OpenPostProcessor extends PickUpPostProcessor {
 	protected String getResourceToOpen(Map<String, String> commandDetails) {
 		String par = commandDetails.get("par");
 		if (par.startsWith("~")) {
-			return System.getProperty("user.home") + "/" + par;
+			return System.getProperty("user.home") + par.substring(1);
 		} else if (!par.startsWith("/")) {
 			String crn = commandDetails.get("crn");
 			return crn + "/" + par;
