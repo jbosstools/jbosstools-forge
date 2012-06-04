998a5f87500bf598ea853a02f7e0051cf76205d1
Koen Aers <koen.aers@jboss.com>
2012-06-04 13:17:06 +0000

    JBIDE-11124: Removing project via forge is not working properly
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41675 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/console/RmPostProcessor.java
+++ plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/console/RmPostProcessor.java
@@ -1,48 +1,48 @@
 package org.jboss.tools.forge.ui.console;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.StringTokenizer;
 
-import org.eclipse.core.filesystem.EFS;
-import org.eclipse.core.filesystem.IFileInfo;
-import org.eclipse.core.filesystem.IFileStore;
-import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
 import org.jboss.tools.forge.ui.ForgeUIPlugin;
 
 
 public class RmPostProcessor implements ForgeCommandPostProcessor {
 	
-	private String getResourceToDelete(Map<String, String> commandDetails) {
-		return commandDetails.get("crn");
+	private ArrayList<String> getResourcesToDelete(Map<String, String> commandDetails) {
+		String crn = commandDetails.get("crn");
+		String par = commandDetails.get("par");
+		int start = par.lastIndexOf('[');
+		int end = par.lastIndexOf(']');
+		if (start == -1 || end == -1) return null;
+		par = par.substring(start + 1, end);
+		ArrayList<String> result = new ArrayList<String>();
+		StringTokenizer tokenizer = new StringTokenizer(par);
+		while (tokenizer.hasMoreTokens()) {
+			result.add(crn + File.separator + tokenizer.nextToken());
+		}
+		return result;
 	}
 	
 	@Override
 	public void postProcess(Map<String, String> commandDetails) {
-		String crn = getResourceToDelete(commandDetails);
-		if (crn == null) return;
-		IPath path = new Path(crn);
-		IFileStore fileStore = EFS.getLocalFileSystem().getStore(new Path(crn));
-		IFileInfo fileInfo = fileStore.fetchInfo();
-		if (!fileInfo.exists()) return;
-		IResource resource = null;
-		if (fileInfo.isDirectory()) {
-			resource = ResourcesPlugin.getWorkspace().getRoot().getContainerForLocation(path);
-		} else {
-			resource = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(path);
-		}
-		if (resource != null) {
-			delete(resource);
-		}
-	}
-	
-	private void delete(IResource resource) {
-		try { 
-			resource.delete(true, null);
-		} catch (CoreException e) {
-			ForgeUIPlugin.log(e);
+		ArrayList<String> resourceNames = getResourcesToDelete(commandDetails);
+		if (resourceNames == null) return;
+		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+		for (String resourceName : resourceNames) {
+			for (IProject project : projects) {
+				if (project.exists() && resourceName.equals(project.getLocation().toOSString())) {
+					try {
+						project.delete(false, false, null);
+					} catch (CoreException e) {
+						ForgeUIPlugin.log(e);
+					}
+				}
+			}
 		}
 	}
 	
