b4911578aeb183a49a269c7ee4d5d58b767efebe
George Gastaldi <gegastaldi@gmail.com>
2013-01-31 01:57:15 -0200

    Changed list to tree dialog

--- plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/handlers/ForgeCommandHandler.java
+++ plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/handlers/ForgeCommandHandler.java
@@ -5,23 +5,14 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.jboss.tools.forge.ui.dialog.UICommandListDialog;
+import org.jboss.tools.forge.ui.dialog.UICommandTreeDialog;
 
 public class ForgeCommandHandler extends AbstractHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil
 				.getActiveWorkbenchWindowChecked(event);
-		UICommandListDialog dialog = new UICommandListDialog(window);
-		return dialog.open();
-		// String message = "";
-		// Set<Addon> addons =
-		// ForgeService.INSTANCE.getAddonRegistry().getRegisteredAddons();
-		// for (Addon addon : addons) {
-		// message += addon + "\n";
-		// }
-		// MessageDialog.openInformation(window.getShell(), "Uiview", message);
-		// return null;
+		return new UICommandTreeDialog(window).open();
 	}
 
 }
