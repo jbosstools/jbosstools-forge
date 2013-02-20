9ec98e07feeb263dfb5c37d6c5f78d651b922ee7
George Gastaldi <gegastaldi@gmail.com>
2013-02-20 09:39:35 -0300

    Validation of each wizard page is now working

--- plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/wizards/ForgeWizardPage.java
+++ plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/wizards/ForgeWizardPage.java
@@ -6,6 +6,7 @@
 
 package org.jboss.tools.forge.ui.wizards;
 
+import java.net.URL;
 import java.util.List;
 
 import org.eclipse.jface.wizard.Wizard;
@@ -13,24 +14,28 @@ import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.jboss.forge.ui.UICommand;
 import org.jboss.forge.ui.UICommandMetadata;
 import org.jboss.forge.ui.input.UIInputComponent;
 import org.jboss.tools.forge.ui.ForgeUIPlugin;
 import org.jboss.tools.forge.ui.context.UIBuilderImpl;
 import org.jboss.tools.forge.ui.context.UIContextImpl;
+import org.jboss.tools.forge.ui.context.UIValidationContextImpl;
 import org.jboss.tools.forge.ui.control.ControlBuilder;
 import org.jboss.tools.forge.ui.control.ControlBuilderRegistry;
 
 /**
  * A Forge Wizard Page
- * 
+ *
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
- * 
+ *
  */
 public class ForgeWizardPage extends WizardPage {
-    private UICommand ui;
+    private UICommand uiCommand;
     private UIContextImpl uiContext;
 
     public ForgeWizardPage(Wizard wizard, UICommand command, UIContextImpl contextImpl) {
@@ -40,15 +45,19 @@ public class ForgeWizardPage extends WizardPage {
         setTitle(id.getName());
         setDescription(id.getDescription());
         setImageDescriptor(ForgeUIPlugin.getForgeLogo());
-        this.ui = command;
+        this.uiCommand = command;
         this.uiContext = contextImpl;
+    }
+
+    public UICommand getUICommand() {
+        return uiCommand;
     }
 
     @Override
     public void createControl(Composite parent) {
         UIBuilderImpl builder = new UIBuilderImpl(uiContext);
         try {
-            ui.initializeUI(builder);
+            uiCommand.initializeUI(builder);
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -72,21 +81,52 @@ public class ForgeWizardPage extends WizardPage {
             Label label = new Label(container, SWT.NULL);
             label.setText(input.getLabel() == null ? input.getName() : input.getLabel());
             ControlBuilder controlBuilder = ControlBuilderRegistry.INSTANCE.getBuilderFor(input);
-            controlBuilder.build(this, (UIInputComponent<?, Object>) input, container);
+            Control control = controlBuilder.build(this, (UIInputComponent<?, Object>) input, container);
+
+            // Update page status
+            Listener pageCompleteListener = new Listener() {
+                @Override
+                public void handleEvent(Event event) {
+                    if (isCurrentPage()) {
+                        isPageComplete();
+                        // Refresh the buttons
+                        getContainer().updateButtons();
+                    }
+                    event.doit = true;
+                }
+            };
+            control.addListener(SWT.Modify, pageCompleteListener);
+            control.addListener(SWT.DefaultSelection, pageCompleteListener);
+            control.addListener(SWT.Selection, pageCompleteListener);
         }
         setControl(container);
     }
 
-    public UIContextImpl getUIContext() {
-        return uiContext;
+    /**
+     * Validates the method
+     */
+    @Override
+    public boolean isPageComplete() {
+        // clear error message
+        setErrorMessage(null);
+        UIValidationContextImpl validationContext = new UIValidationContextImpl(uiContext);
+        // invokes the validation in the current UICommand
+        uiCommand.validate(validationContext);
+        List<String> errors = validationContext.getErrors();
+        boolean noErrors = errors.isEmpty();
+        if (!noErrors) {
+            setErrorMessage(errors.get(0));
+        }
+        // if no errors were found, the page is ready to go to the next step
+        return noErrors;
     }
 
-    public UICommand getUICommand() {
-        return ui;
-    }
-
-    public void updateStatus(String message) {
-        setErrorMessage(message);
-        setPageComplete(message == null);
+    @Override
+    public void performHelp() {
+        UICommandMetadata metadata = uiCommand.getMetadata();
+        URL docLocation = metadata.getDocLocation();
+        if (docLocation != null) {
+//            PlatformUI.getWorkbench().getHelpSystem().displayHelp(docLocation.toExternalForm());
+        }
     }
 }
