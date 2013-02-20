e753ab856f2957f433ef7d1a4fb996388d664a58
George Gastaldi <gegastaldi@gmail.com>
2013-02-19 21:39:13 -0300

    Updated plugin to use NavigationResult

--- plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/wizards/ForgeMultiStepWizard.java
+++ plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/wizards/ForgeMultiStepWizard.java
@@ -3,43 +3,66 @@ package org.jboss.tools.forge.ui.wizards;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.jboss.forge.ui.UICommand;
+import org.jboss.forge.ui.result.NavigationResult;
 import org.jboss.forge.ui.wizard.UIWizard;
 import org.jboss.tools.forge.core.ForgeService;
 
-public class ForgeMultiStepWizard extends ForgeWizard {
+public class ForgeMultiStepWizard extends ForgeWizard
+{
 
-	public ForgeMultiStepWizard(UIWizard stepBegin,
-			IStructuredSelection selection) {
-		super(stepBegin, selection);
-		setForcePreviousAndNextButtons(true);
-	}
+   public ForgeMultiStepWizard(UIWizard stepBegin,
+            IStructuredSelection selection)
+   {
+      super(stepBegin, selection);
+      setForcePreviousAndNextButtons(true);
+   }
 
-	@Override
-	public IWizardPage getNextPage(IWizardPage page) {
-		UICommand uiCommand = ((ForgeWizardPage) page).getUICommand();
-		UIWizard wiz = (UIWizard) uiCommand;
-		Class<? extends UIWizard> successor = wiz.getSuccessor();
-		// No next page
-		if (successor == null) {
-			return null;
-		} else {
-			// Do we have any pages already displayed ? (Did we went back
-			// already ?)
-			ForgeWizardPage nextPage = (ForgeWizardPage) super
-					.getNextPage(page);
-			if (nextPage == null) {
-				UIWizard nextStep = ForgeService.INSTANCE.lookup(successor);
-				nextPage = new ForgeWizardPage(this, nextStep, getUiContext());
-				addPage(nextPage);
-			}
-			return nextPage;
-		}
-	}
+   @Override
+   public IWizardPage getNextPage(IWizardPage page)
+   {
+      UICommand uiCommand = ((ForgeWizardPage) page).getUICommand();
+      if (!(uiCommand instanceof UIWizard))
+      {
+         return null;
+      }
+      UIWizard wiz = (UIWizard) uiCommand;
+      NavigationResult nextCommand = null;
+      try
+      {
+         nextCommand = wiz.next(getUiContext());
+      }
+      catch (Exception e)
+      {
+         // TODO: Use Eclipse logging mechanism
+         e.printStackTrace();
+      }
+      // No next page
+      if (nextCommand == null)
+      {
+         return null;
+      }
+      else
+      {
+         Class<? extends UICommand> successor = nextCommand.getNext();
+         // Do we have any pages already displayed ? (Did we went back
+         // already ?)
+         ForgeWizardPage nextPage = (ForgeWizardPage) super
+                  .getNextPage(page);
+         if (nextPage == null)
+         {
+            UICommand nextStep = ForgeService.INSTANCE.lookup(successor);
+            nextPage = new ForgeWizardPage(this, nextStep, getUiContext());
+            addPage(nextPage);
+         }
+         return nextPage;
+      }
+   }
 
-	@Override
-	public boolean performFinish() {
-		// TODO Auto-generated method stub
-		return super.performFinish();
-	}
+   @Override
+   public boolean performFinish()
+   {
+      // TODO Auto-generated method stub
+      return super.performFinish();
+   }
 
 }
