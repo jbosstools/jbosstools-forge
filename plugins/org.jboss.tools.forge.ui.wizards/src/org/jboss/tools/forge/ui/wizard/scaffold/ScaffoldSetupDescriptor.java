df9475a2e228c47832d8d18111b30ef3f2c8f056
Koen Aers <koen.aers@gmail.com>
2013-03-06 17:01:13 +0100

    add a scaffold setup wizard

---
+++ plugins/org.jboss.tools.forge.ui.wizards/src/org/jboss/tools/forge/ui/wizard/scaffold/ScaffoldSetupDescriptor.java
@@ -0,0 +1,8 @@
+package org.jboss.tools.forge.ui.wizard.scaffold;
+
+
+public class ScaffoldSetupDescriptor {
+	
+	public String project;
+
+}
---
+++ plugins/org.jboss.tools.forge.ui.wizards/src/org/jboss/tools/forge/ui/wizard/scaffold/ScaffoldSetupWizardPage.java
@@ -0,0 +1,61 @@
+package org.jboss.tools.forge.ui.wizard.scaffold;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.jboss.tools.forge.ui.wizard.dialog.JPAProjectSelectionDialog;
+
+public class ScaffoldSetupWizardPage extends WizardPage {
+	
+	private ScaffoldSetupDescriptor scaffoldSetupDescriptor = new ScaffoldSetupDescriptor();
+
+	protected ScaffoldSetupWizardPage() {
+		super("org.jboss.tools.forge.ui.wizard.scaffold.setup", "Set Up Scaffolding", null);
+	}
+
+	@Override
+	public void createControl(Composite parent) {
+		Composite control = new Composite(parent, SWT.NULL);
+		control.setLayout(new GridLayout(3, false));
+		createProjectEditor(control);
+		setControl(control);
+	}
+	
+	private void createProjectEditor(Composite parent) {
+		Label projectNameLabel = new Label(parent, SWT.NONE);
+		projectNameLabel.setText("Project: ");
+		final Text projectNameText = new Text(parent, SWT.BORDER);
+		projectNameText.setText("");
+		projectNameText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		Button projectSearchButton = new Button(parent, SWT.NONE);
+		projectSearchButton.setText("Search...");
+		projectSearchButton.addSelectionListener(new SelectionListener() {			
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				JPAProjectSelectionDialog dialog = new JPAProjectSelectionDialog(getShell());
+				if (dialog.open() != SWT.CANCEL) {
+					IProject project = (IProject)dialog.getResult()[0];
+					projectNameText.setText(project.getName());
+					scaffoldSetupDescriptor.project = project.getName();
+				}
+			}			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				widgetSelected(e);
+			}
+		});
+	}
+		
+	public ScaffoldSetupDescriptor getScaffoldSetupDescriptor() {
+		return scaffoldSetupDescriptor;
+	}
+	
+}
