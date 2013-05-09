a842b90c6a9fe54ced6cf3dcb2f1e9b53962ddc2
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-05-09 10:30:38 -0400

    Try to be a little nicer to the Forge container, and make sure that it gets a chance to start before asking it too many questions.

--- plugins/org.jboss.tools.forge.core.ext/src/org/jboss/tools/forge/ext/core/ForgeService.java
+++ plugins/org.jboss.tools.forge.core.ext/src/org/jboss/tools/forge/ext/core/ForgeService.java
@@ -7,61 +7,74 @@
 
 package org.jboss.tools.forge.ext.core;
 
+import java.util.concurrent.Callable;
+
 import org.jboss.forge.container.ContainerStatus;
 import org.jboss.forge.container.Forge;
 import org.jboss.forge.container.addons.AddonRegistry;
+import org.jboss.forge.container.lock.LockMode;
 import org.jboss.forge.container.services.ExportedInstance;
 import org.jboss.forge.convert.ConverterFactory;
 
 /**
  * This is a singleton for the {@link Forge} class.
- *
+ * 
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
- *
+ * 
  */
 
 public enum ForgeService {
-    INSTANCE;
+	INSTANCE;
 
-    private transient Forge forge;
-    private ConverterFactory converterFactory;
+	private transient Forge forge;
+	private ConverterFactory converterFactory;
+	boolean started = false;
 
-    private ForgeService() {
-    }
-
-    public void setForge(Forge forge) {
-	this.forge = forge;
-    }
-
-    public void start(ClassLoader loader) {
-	forge.startAsync(loader);
-    }
-
-    public AddonRegistry getAddonRegistry() {
-	return forge.getAddonRegistry();
-    }
-
-    public void stop() {
-	forge.stop();
-    }
-
-    public ContainerStatus getContainerStatus() {
-	return (forge == null) ? ContainerStatus.STOPPED : forge.getStatus();
-    }
-
-    public ConverterFactory getConverterFactory() {
-	if (converterFactory == null) {
-	    converterFactory = lookup(ConverterFactory.class);
+	private ForgeService() {
 	}
-	return converterFactory;
-    }
 
-    public <S> S lookup(Class<S> service) {
-	ExportedInstance<S> exportedInstance = null;
-	if (forge != null) {
-	    exportedInstance = forge.getAddonRegistry().getExportedInstance(
-		    service);
+	public void setForge(Forge forge) {
+		this.forge = forge;
 	}
-	return (exportedInstance == null) ? null : exportedInstance.get();
-    }
+
+	public void start(final ClassLoader loader) {
+		forge.getLockManager().performLocked(LockMode.WRITE,
+				new Callable<Void>() {
+					@Override
+					public Void call() throws Exception {
+						forge.startAsync(loader);
+						return null;
+					}
+				});
+	}
+
+	public AddonRegistry getAddonRegistry() {
+		return forge.getAddonRegistry();
+	}
+
+	public void stop() {
+		forge.stop();
+		started = false;
+	}
+
+	public ContainerStatus getContainerStatus() {
+		return (forge == null || !started) ? ContainerStatus.STOPPED : forge
+				.getStatus();
+	}
+
+	public ConverterFactory getConverterFactory() {
+		if (converterFactory == null) {
+			converterFactory = lookup(ConverterFactory.class);
+		}
+		return converterFactory;
+	}
+
+	public <S> S lookup(Class<S> service) {
+		ExportedInstance<S> exportedInstance = null;
+		if (forge != null) {
+			exportedInstance = forge.getAddonRegistry().getExportedInstance(
+					service);
+		}
+		return (exportedInstance == null) ? null : exportedInstance.get();
+	}
 }
