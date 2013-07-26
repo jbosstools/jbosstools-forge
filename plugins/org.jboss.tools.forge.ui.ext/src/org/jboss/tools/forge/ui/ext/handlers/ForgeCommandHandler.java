dd6eae68b7c13323d7b21e8f67724b400ca3d120
Koen Aers <koen.aers@gmail.com>
2013-07-26 14:51:03 +0200

    even better: add progress monitor while F2 is starting

--- plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/handlers/ForgeCommandHandler.java
+++ plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/handlers/ForgeCommandHandler.java
@@ -1,70 +1,51 @@
 package org.jboss.tools.forge.ui.ext.handlers;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
-import org.eclipse.core.runtime.jobs.IJobChangeListener;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
-import org.eclipse.swt.widgets.Display;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.jboss.tools.forge.ext.core.FurnaceService;
 import org.jboss.tools.forge.ui.ext.ForgeUIPlugin;
 import org.jboss.tools.forge.ui.ext.dialog.UICommandListDialog;
+import org.jboss.tools.forge.ui.ext.dialog.WizardDialogHelper;
 
-/**
- * Called when CTRL (Or Command in OSX) + 5 is pressed
- */
 public class ForgeCommandHandler extends AbstractHandler {
-
-	private ExecutionEvent event;
-
-	private Job checkFurnaceStatusJob = new Job("Check Furnace Status") {
-		@Override
-		protected IStatus run(IProgressMonitor arg0) {
-			try {
-				FurnaceService.INSTANCE.waitUntilContainerIsStarted();
-			} catch (InterruptedException e) {
-				ForgeUIPlugin.log(e);
-			}
-			return Status.OK_STATUS;
-		}
-	};
-
-	private IJobChangeListener jobChangeListener = new JobChangeAdapter() {
-		@Override
-		public void done(IJobChangeEvent arg0) {
-			checkFurnaceStatusJob.removeJobChangeListener(jobChangeListener);
-			openUICommandListDialog();
-		}
-	};
-
+	
+	private Shell shell;
+	
 	@Override
 	public Object execute(ExecutionEvent event) {
-		this.event = event;
-		checkFurnaceStatusJob.addJobChangeListener(jobChangeListener);
-		checkFurnaceStatusJob.schedule();
+		try {
+			shell = HandlerUtil.getActiveShell(event);
+			final IWorkbenchWindow window = HandlerUtil
+					.getActiveWorkbenchWindowChecked(event);
+			ProgressMonitorDialog pmd = 
+					new ProgressMonitorDialog(HandlerUtil.getActiveShell(event));
+			pmd.run(true, false, new IRunnableWithProgress() {				
+				@Override
+				public void run(IProgressMonitor arg0) throws InvocationTargetException,
+						InterruptedException {
+					FurnaceService.INSTANCE.waitUntilContainerIsStarted();
+					// hack to make progress monitor stick until all commands are loaded
+					ISelection selection = window.getSelectionService().getSelection();
+					if (selection instanceof IStructuredSelection) {
+						new WizardDialogHelper(shell, (IStructuredSelection)selection).getAllCandidatesAsMap();
+					}
+				}
+			});
+			new UICommandListDialog(window).open();
+		} catch (Exception e) {
+			ForgeUIPlugin.log(e);
+		}
 		return null;
 	}
-
-	private void openUICommandListDialog() {
-		Display.getDefault().asyncExec(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					IWorkbenchWindow window = HandlerUtil
-							.getActiveWorkbenchWindowChecked(event);
-					new UICommandListDialog(window).open();
-				} catch (ExecutionException e) {
-					ForgeUIPlugin.log(e);
-				}
-			}
-		});
-	}
-
+		
 }
