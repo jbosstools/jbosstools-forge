c0d5098a11f99ddfe862b3294ebc0c5703499a0a
George Gastaldi <gegastaldi@gmail.com>
2013-08-22 09:52:52 -0300

    Update to Aesh 0.40

--- plugins/org.jboss.tools.aesh.core/src/org/jboss/tools/aesh/core/console/AeshConsole.java
+++ plugins/org.jboss.tools.aesh.core/src/org/jboss/tools/aesh/core/console/AeshConsole.java
@@ -20,20 +20,20 @@ import org.jboss.tools.aesh.core.io.AeshOutputStream;
 import org.jboss.tools.aesh.core.io.AeshOutputStream.StreamListener;
 
 public class AeshConsole {
-	
+
 	private AeshInputStream inputStream;
 	private AeshOutputStream stdOut, stdErr;
 	private Console console;
-	
+
 	public AeshConsole() {
 		initialize();
 	}
-	
+
 	protected void initialize() {
 		createStreams();
 		createConsole();
 	}
-	
+
 	protected void createConsole() {
 		try {
 			console = new Console(createAeshSettings());
@@ -43,51 +43,48 @@ public class AeshConsole {
 			e.printStackTrace();
 		}
 	}
-	
+
 	protected Settings createAeshSettings() {
-		return new SettingsBuilder()
-			.inputStream(inputStream)
-			.outputStream(stdOut)
-			.outputStreamError(stdErr)
-			.create();
+		return new SettingsBuilder().inputStream(inputStream)
+				.outputStream(stdOut).outputStreamError(stdErr).create();
 	}
-	
+
 	protected void createStreams() {
 		inputStream = new AeshInputStream();
 		stdOut = new AeshOutputStream();
 		stdErr = new AeshOutputStream();
 	}
-	
+
 	private Prompt createPrompt() {
-        List<TerminalCharacter> chars = new ArrayList<TerminalCharacter>();
-        chars.add(new TerminalCharacter('[', Color.DEFAULT_BG, Color.BLUE_TEXT));
-        chars.add(new TerminalCharacter('t', Color.DEFAULT_BG, Color.RED_TEXT,
-                CharacterType.ITALIC));
-        chars.add(new TerminalCharacter('e', Color.DEFAULT_BG, Color.RED_TEXT,
-                CharacterType.INVERT));
-        chars.add(new TerminalCharacter('s', Color.DEFAULT_BG, Color.RED_TEXT,
-                CharacterType.CROSSED_OUT));
-        chars.add(new TerminalCharacter('t', Color.DEFAULT_BG ,Color.RED_TEXT,
-                CharacterType.BOLD));
-        chars.add(new TerminalCharacter(']', Color.DEFAULT_BG, Color.BLUE_TEXT,
-                CharacterType.PLAIN));
-        chars.add(new TerminalCharacter('$', Color.DEFAULT_BG, Color.WHITE_TEXT,
-                CharacterType.UNDERLINE));
-        chars.add(new TerminalCharacter(' ', Color.DEFAULT_BG, Color.WHITE_TEXT));
-        return new Prompt(chars);
+		List<TerminalCharacter> chars = new ArrayList<TerminalCharacter>();
+		chars.add(new TerminalCharacter('[', Color.DEFAULT_BG, Color.BLUE_TEXT));
+		chars.add(new TerminalCharacter('t', Color.DEFAULT_BG, Color.RED_TEXT,
+				CharacterType.ITALIC));
+		chars.add(new TerminalCharacter('e', Color.DEFAULT_BG, Color.RED_TEXT,
+				CharacterType.INVERT));
+		chars.add(new TerminalCharacter('s', Color.DEFAULT_BG, Color.RED_TEXT,
+				CharacterType.CROSSED_OUT));
+		chars.add(new TerminalCharacter('t', Color.DEFAULT_BG, Color.RED_TEXT,
+				CharacterType.BOLD));
+		chars.add(new TerminalCharacter(']', Color.DEFAULT_BG, Color.BLUE_TEXT,
+				CharacterType.PLAIN));
+		chars.add(new TerminalCharacter('$', Color.DEFAULT_BG,
+				Color.WHITE_TEXT, CharacterType.UNDERLINE));
+		chars.add(new TerminalCharacter(' ', Color.DEFAULT_BG, Color.WHITE_TEXT));
+		return new Prompt(chars);
 	}
-	
+
 	private ConsoleCallback createConsoleCallback() {
 		return new ConsoleCallback() {
 			@Override
 			public int readConsoleOutput(ConsoleOutput output)
 					throws IOException {
-				console.pushToStdOut("hoorray\n");
+				console.out().println("hoorray");
 				return 0;
 			}
 		};
 	}
-	
+
 	public void start() {
 		try {
 			console.start();
@@ -99,7 +96,7 @@ public class AeshConsole {
 	public void sendInput(String input) {
 		inputStream.append(input);
 	}
-	
+
 	public void stop() {
 		try {
 			console.stop();
@@ -107,33 +104,33 @@ public class AeshConsole {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void addStdOutListener(StreamListener listener) {
 		stdOut.addStreamListener(listener);
 	}
-	
+
 	public void removeStdOutListener(StreamListener listener) {
 		stdOut.removeStreamListener(listener);
 	}
-	
+
 	public void addStdErrListener(StreamListener listener) {
 		stdErr.addStreamListener(listener);
 	}
-	
+
 	public void removeStdErrListener(StreamListener listener) {
 		stdErr.removeStreamListener(listener);
 	}
-	
+
 	protected InputStream getInputStream() {
 		return inputStream;
 	}
-	
+
 	protected OutputStream getStdOut() {
 		return stdOut;
 	}
-	
+
 	protected OutputStream getStdErr() {
 		return stdErr;
 	}
-	
+
 }
