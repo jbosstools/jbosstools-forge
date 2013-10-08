1fa25cf7f6f4b5a25c3ac5b0341d961f3808b6f1
George Gastaldi <gegastaldi@gmail.com>
2013-10-08 16:43:38 -0300

    Organized imports

--- plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/control/ComboControlBuilder.java
+++ plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/control/ComboControlBuilder.java
@@ -9,9 +9,7 @@ package org.jboss.tools.forge.ui.ext.control;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
