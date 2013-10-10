0313ffa1113f45ab7701b365d7932aea5657bda6
George Gastaldi <gegastaldi@gmail.com>
2013-10-10 17:55:53 -0300

    FORGE-1256: Fixed ComboBox recursion problem

--- plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/control/ComboControlBuilder.java
+++ plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/control/ComboControlBuilder.java
@@ -9,7 +9,9 @@ package org.jboss.tools.forge.ui.ext.control;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
@@ -28,37 +30,38 @@ import org.jboss.tools.forge.ext.core.FurnaceService;
 import org.jboss.tools.forge.ui.ext.wizards.ForgeWizardPage;
 
 public class ComboControlBuilder extends ControlBuilder<Combo> {
-	
+
+	private static final Set<Combo> COMBO_STATUS_CHANGE = new HashSet<Combo>();
+
 	@Override
 	public Combo build(ForgeWizardPage page,
 			final InputComponent<?, Object> input, final Composite container) {
 
-	   // Create the label
+		// Create the label
 		Label label = new Label(container, SWT.NULL);
 		label.setText(getMnemonicLabel(input, true));
 
 		final Combo combo = new Combo(container, SWT.BORDER | SWT.DROP_DOWN
 				| SWT.READ_ONLY);
 		combo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		combo.addModifyListener(new ModifyListener() {			
+		combo.addModifyListener(new ModifyListener() {
 			@Override
 			public void modifyText(ModifyEvent e) {
 				InputComponents.setValueFor(
-						FurnaceService.INSTANCE.getConverterFactory(), 
-						input,
+						FurnaceService.INSTANCE.getConverterFactory(), input,
 						combo.getText());
 			}
 		});
 		combo.setToolTipText(input.getDescription());
-		
+
 		// skip the third column
 		Label dummy = new Label(container, SWT.NONE);
 		dummy.setText("");
-		
+
 		updateValues(combo, input);
 		return combo;
 	}
-	
+
 	@Override
 	public void setEnabled(Combo control, boolean enabled) {
 		if (enabled != control.isEnabled()) {
@@ -85,58 +88,66 @@ public class ComboControlBuilder extends ControlBuilder<Combo> {
 	public Control[] getModifiableControlsFor(Combo control) {
 		return new Control[] { control };
 	}
-	
+
 	@Override
 	public void updateState(Combo combo, InputComponent<?, Object> input) {
 		super.updateState(combo, input);
 		updateValues(combo, input);
 	}
-	
+
 	private Converter<Object, String> getConverter(UISelectOne<Object> selectOne) {
-		return (Converter<Object, String>)InputComponents.getItemLabelConverter(
-				FurnaceService.INSTANCE.getConverterFactory(), selectOne);
+		return (Converter<Object, String>) InputComponents
+				.getItemLabelConverter(
+						FurnaceService.INSTANCE.getConverterFactory(),
+						selectOne);
 	}
-	
+
 	private void updateDefaultValue(Combo combo, UISelectOne<Object> selectOne) {
 		Converter<Object, String> converter = getConverter(selectOne);
-		String value = converter.convert(InputComponents.getValueFor(selectOne));
+		String value = converter
+				.convert(InputComponents.getValueFor(selectOne));
 		if (value == null) {
 			combo.setText("");
-		} else if (!value.equals(combo.getText())){
+		} else if (!value.equals(combo.getText())) {
 			combo.setText(value);
 		}
 	}
-	
+
 	private void updateValueChoices(Combo combo, UISelectOne<Object> selectOne) {
-		List<String> newItems = new ArrayList<String>();
-		List<String> oldItems = Arrays.asList(combo.getItems());
-		boolean changed = false;
-		Iterable<Object> valueChoices = selectOne.getValueChoices();
-		Converter<Object, String> converter = getConverter(selectOne);
-		if (valueChoices != null) {
-			for (Object choice : valueChoices) {
-				String itemLabel = converter.convert(choice);
-				newItems.add(itemLabel);
-				if (!oldItems.contains(itemLabel)) {
-					changed = true;
+		if (!COMBO_STATUS_CHANGE.add(combo)) {
+			return;
+		}
+		try {
+			String selectedText = combo.getText();
+			List<String> newItems = new ArrayList<String>();
+			List<String> oldItems = Arrays.asList(combo.getItems());
+			Iterable<Object> valueChoices = selectOne.getValueChoices();
+			Converter<Object, String> converter = getConverter(selectOne);
+			if (valueChoices != null) {
+				for (Object choice : valueChoices) {
+					String itemLabel = converter.convert(choice);
+					newItems.add(itemLabel);
 				}
 			}
-		} else if (!oldItems.isEmpty()) {
-			changed = true;
-		}
-		if (changed) {
-			combo.removeAll();
-			for (String label : newItems) {
-				combo.add(label);
+			if (!Arrays.equals(newItems.toArray(), oldItems.toArray())) {
+				combo.removeAll();
+				for (String label : newItems) {
+					combo.add(label);
+					if (label.equals(selectedText)) {
+						combo.setText(label);
+					}
+				}
 			}
+		} finally {
+			COMBO_STATUS_CHANGE.remove(combo);
 		}
 	}
-		
+
 	@SuppressWarnings("unchecked")
 	private void updateValues(Combo combo, InputComponent<?, Object> input) {
-		UISelectOne<Object> selectOne = (UISelectOne<Object>)input;
+		UISelectOne<Object> selectOne = (UISelectOne<Object>) input;
 		updateValueChoices(combo, selectOne);
 		updateDefaultValue(combo, selectOne);
 	}
-	
+
 }
