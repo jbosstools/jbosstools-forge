3c41e385030ecd9b7069d8bd32c4617c47ecd6e2
George Gastaldi <gegastaldi@gmail.com>
2013-10-17 22:17:55 -0300

    Organized imports

--- plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/cli/F2Console.java
+++ plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/cli/F2Console.java
@@ -1,29 +1,31 @@
 package org.jboss.tools.forge.ui.ext.cli;
 
-import org.jboss.aesh.console.settings.Settings;
-import org.jboss.aesh.console.settings.SettingsBuilder;
-import org.jboss.forge.addon.shell.Shell;
-import org.jboss.forge.addon.shell.ShellFactory;
-import org.jboss.forge.furnace.util.OperatingSystemUtils;
 import org.jboss.tools.aesh.core.console.AeshConsole;
-import org.jboss.tools.forge.ext.core.FurnaceService;
 
 public class F2Console extends AeshConsole {
+	// private Shell shell;
 
 	protected void createConsole() {
 		super.createConsole();
 		// ShellFactory shellFactory = FurnaceService.INSTANCE
 		// .lookup(ShellFactory.class);
-		// Settings settings = new
-		// SettingsBuilder().inputStream(getInputStream())
-		// .outputStream(getStdOut()).outputStreamError(getStdErr())
-		// .create();
-		// Shell shell = shellFactory.createShell(
-		// OperatingSystemUtils.getUserHomeDir(), settings);
+		// shell =
+		// shellFactory.createShell(OperatingSystemUtils.getUserHomeDir(),
+		// createAeshSettings());
 	}
 
 	public void start() {
 		// Already started
+		// if (shell == null)
+		// createConsole();
+
 		super.start();
+	}
+
+	@Override
+	public void stop() {
+		super.stop();
+		// shell.close();
+		// shell = null;
 	}
 }
