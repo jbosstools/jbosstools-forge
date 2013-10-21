0ae4fcf743ebdc549d322ea5e568737ba6df7caa
George Gastaldi <gegastaldi@gmail.com>
2013-10-21 19:36:34 -0200

    Uncommented F2Console code

--- plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/cli/F2Console.java
+++ plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/cli/F2Console.java
@@ -1,31 +1,33 @@
 package org.jboss.tools.forge.ui.ext.cli;
 
+import org.jboss.forge.addon.shell.Shell;
+import org.jboss.forge.addon.shell.ShellFactory;
+import org.jboss.forge.furnace.util.OperatingSystemUtils;
 import org.jboss.tools.aesh.core.console.AeshConsole;
+import org.jboss.tools.forge.ext.core.FurnaceService;
 
 public class F2Console extends AeshConsole {
-	// private Shell shell;
+	private Shell shell;
 
 	protected void createConsole() {
-		super.createConsole();
-		// ShellFactory shellFactory = FurnaceService.INSTANCE
-		// .lookup(ShellFactory.class);
-		// shell =
-		// shellFactory.createShell(OperatingSystemUtils.getUserHomeDir(),
-		// createAeshSettings());
+		// super.createConsole();
+		ShellFactory shellFactory = FurnaceService.INSTANCE
+				.lookup(ShellFactory.class);
+		shell = shellFactory.createShell(OperatingSystemUtils.getUserHomeDir(),
+				createAeshSettings());
 	}
 
 	public void start() {
 		// Already started
-		// if (shell == null)
-		// createConsole();
+		if (shell == null)
+			createConsole();
 
-		super.start();
+//		super.start();
 	}
 
 	@Override
 	public void stop() {
-		super.stop();
-		// shell.close();
-		// shell = null;
+		shell.close();
+		shell = null;
 	}
 }
