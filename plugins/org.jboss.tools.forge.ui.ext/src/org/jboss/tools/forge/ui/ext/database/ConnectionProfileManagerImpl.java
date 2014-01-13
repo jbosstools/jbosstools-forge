0fef94ddc4024d0f89562cf81b8341a5baf41b0c
Koen Aers <koen.aers@gmail.com>
2014-01-13 18:21:41 +0100

    add & remove connection profile is working in eclipse

--- plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/database/ConnectionProfileManagerImpl.java
+++ plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/database/ConnectionProfileManagerImpl.java
@@ -8,6 +8,8 @@ import java.util.Properties;
 import org.eclipse.datatools.connectivity.ConnectionProfileException;
 import org.eclipse.datatools.connectivity.IConnectionProfile;
 import org.eclipse.datatools.connectivity.ProfileManager;
+import org.eclipse.datatools.connectivity.drivers.DriverInstance;
+import org.eclipse.datatools.connectivity.drivers.DriverManager;
 import org.jboss.forge.addon.database.tools.connections.ConnectionProfile;
 import org.jboss.forge.addon.database.tools.connections.ConnectionProfileManager;
 import org.jboss.tools.forge.ui.ext.ForgeUIPlugin;
@@ -28,6 +30,9 @@ public class ConnectionProfileManagerImpl implements ConnectionProfileManager {
 	private static final String VENDOR = "org.eclipse.datatools.connectivity.db.vendor";
 	private static final String HIBERNATE_DIALECT = "org.jboss.tools.forge.hibernate.dialect";
 	
+	private static final String DRIVER_TEMPLATE = "org.eclipse.datatools.connectivity.db.generic.genericDriverTemplate";
+	private static final String PROVIDER_ID = "org.eclipse.datatools.connectivity.db.generic.connectionProfile";
+	
 	@Override
 	public Map<String, ConnectionProfile> loadConnectionProfiles() {
 		IConnectionProfile[] connectionProfiles = ProfileManager
@@ -47,50 +52,125 @@ public class ConnectionProfileManagerImpl implements ConnectionProfileManager {
 		}
 		return result;		
 	}
-
-	@Override
-	public void saveConnectionProfiles(Collection<ConnectionProfile> connectionProfiles) {
+	
+	public void saveConnectionProfile(ConnectionProfile profile) {
+		DriverInstance driverInstance = 
+				DriverManager.getInstance().getDriverInstanceByName(profile.getName());
+		if (driverInstance != null) {
+			saveExistingDriver(driverInstance, profile);
+		}
+		IConnectionProfile connectionProfile = 
+				ProfileManager.getInstance().getProfileByName(profile.getName());
+		if (connectionProfile != null) {
+			saveExistingProfile(profile, connectionProfile);
+		} else {
+			createNewProfile(profile);
+		}
+	}
+	
+	private void saveExistingProfile(ConnectionProfile source, IConnectionProfile target) {
 		try {
-			for (ConnectionProfile profile : connectionProfiles) {
-				IConnectionProfile connectionProfile = 
-						ProfileManager.getInstance().getProfileByName(profile.getName());
-				Properties baseProps = new Properties();
-				baseProps.setProperty(CONNECTION_PROPERTIES, "");
-				baseProps.setProperty(SAVE_PWD, "false");
-				baseProps.setProperty(
-						DRIVER_DEFINITION_TYPE, 
-						"org.eclipse.datatools.connectivity.db.generic.genericDriverTemplate");
-				baseProps.setProperty(DRIVER_LOCATION, profile.getPath());
-				baseProps.setProperty(USER_NAME, profile.getUser());
-				baseProps.setProperty(DRIVER_CLASS, profile.getDriver());
-				baseProps.setProperty(
-						DRIVER_DEFINITION_ID, 
-						"DriverDefn.org.eclipse.datatools.connectivity.db.generic.genericDriverTemplate.Generic JDBC Driver");
-				baseProps.setProperty(DATABASE_NAME, "SAMPLE");
-				baseProps.setProperty(PASSWORD, "");
-				baseProps.setProperty(URL, profile.getUrl());
-				baseProps.setProperty(VERSION, "1.0");
-				baseProps.setProperty(VENDOR, "Generic JDBC");
-				if (profile.getDialect() != null) {
-					baseProps.setProperty(HIBERNATE_DIALECT, profile.getDialect());
-				}
-				if (connectionProfile == null) {
-						connectionProfile = 
-								ProfileManager.getInstance().createProfile(
-										profile.getName(), 
-										"", 
-										"org.eclipse.datatools.connectivity.db.generic.connectionProfile", 
-										baseProps, 
-										"", 
-										false);
-				} else {
-					connectionProfile.setBaseProperties(baseProps);
-					ProfileManager.getInstance().modifyProfile(connectionProfile);
-				}
+			Properties newProperties = createProperties(source);
+			if (propertiesChanged(target.getBaseProperties(), newProperties)) {
+				target.setBaseProperties(newProperties);
+					ProfileManager.getInstance().modifyProfile(target);
 			}
 		} catch (ConnectionProfileException e) {
 			ForgeUIPlugin.log(e);
 		}
+	}
+	
+	private void saveExistingDriver(DriverInstance driverInstance, ConnectionProfile profile) {
+		if (!profile.getPath().equals(driverInstance.getJarList()) || 
+				!profile.getDriver().equals(driverInstance.getProperty(DRIVER_CLASS))) {
+			System.out.println("saving existing driver");
+			DriverManager.getInstance().removeDriverInstance(driverInstance.getId());
+			DriverManager.getInstance().createNewDriverInstance(
+					DRIVER_TEMPLATE, 
+					profile.getName(), 
+					profile.getPath(), 
+					profile.getDriver());
+		}
+	}
+	
+	private boolean propertiesChanged(Properties oldProps, Properties newProps) {
+		boolean result = false;
+		result = result || oldProps.getProperty(DRIVER_CLASS).equals(newProps.getProperty(DRIVER_CLASS));
+		result = result || oldProps.getProperty(DRIVER_LOCATION).equals(newProps.getProperty(DRIVER_LOCATION));
+		result = result || oldProps.getProperty(USER_NAME).equals(newProps.getProperty(USER_NAME));
+		result = result || oldProps.getProperty(URL).equals(newProps.getProperty(URL));
+		result = result || oldProps.getProperty(HIBERNATE_DIALECT).equals(newProps.getProperty(HIBERNATE_DIALECT));
+		return result;
+	}
+	
+	private void createNewProfile(ConnectionProfile profile) {
+		try {
+			DriverManager.getInstance().createNewDriverInstance(
+					DRIVER_TEMPLATE, 
+					profile.getName(), 
+					profile.getPath(), 
+					profile.getDriver());
+			ProfileManager.getInstance().createProfile(
+					profile.getName(), 
+					"", 
+					PROVIDER_ID, 
+					createProperties(profile), 
+					"", 
+					false);
+		} catch (ConnectionProfileException e) {
+			ForgeUIPlugin.log(e);
+		}
+	}
+
+	@Override
+	public void saveConnectionProfiles(Collection<ConnectionProfile> connectionProfiles) {
+		Map<String, ConnectionProfile> existingProfiles = loadConnectionProfiles();
+		for (ConnectionProfile profile : connectionProfiles) {
+			existingProfiles.remove(profile.getName());
+			saveConnectionProfile(profile);
+		}
+		for (String name : existingProfiles.keySet()) {
+			deleteConnectionProfile(name);
+		}
+	}
+	
+	private void deleteConnectionProfile(String name) {
+		try {
+			DriverInstance driverInstance = DriverManager.getInstance().getDriverInstanceByName(name);
+			if (driverInstance != null) {
+				DriverManager.getInstance().removeDriverInstance(driverInstance.getId());
+			}
+			IConnectionProfile profile = ProfileManager.getInstance().getProfileByName(name);
+			if (profile != null) {
+				ProfileManager.getInstance().deleteProfile(profile);
+			}
+		} catch (ConnectionProfileException e) {
+			ForgeUIPlugin.log(e);
+		}
+	}
+	
+	private Properties createProperties(ConnectionProfile profile) {
+		Properties result = new Properties();
+		result.setProperty(CONNECTION_PROPERTIES, "");
+		result.setProperty(SAVE_PWD, "false");
+		result.setProperty(DRIVER_DEFINITION_TYPE, DRIVER_TEMPLATE);
+		result.setProperty(DRIVER_LOCATION, profile.getPath());
+		result.setProperty(USER_NAME, profile.getUser());
+		result.setProperty(DRIVER_CLASS, profile.getDriver());
+		result.setProperty(DRIVER_DEFINITION_ID, getDriver(profile.getName()).getId());
+		result.setProperty(DATABASE_NAME, profile.getName());
+		result.setProperty(PASSWORD, profile.getPassword());
+		result.setProperty(URL, profile.getUrl());
+		result.setProperty(VERSION, "1.0");
+		result.setProperty(VENDOR, "Generic JDBC");
+		if (profile.getDialect() != null) {
+			result.setProperty(HIBERNATE_DIALECT, profile.getDialect());
+		}
+		return result;
+	}
+	
+	private DriverInstance getDriver(String name) {
+		return DriverManager.getInstance().getDriverInstanceByName(name);
 	}
 
 }
