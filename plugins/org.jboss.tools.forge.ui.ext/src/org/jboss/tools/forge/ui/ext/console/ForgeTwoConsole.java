c14bcdcd270b676cd09b34fc137d4f1d27e2aebd
Koen Aers <koen.aers@gmail.com>
2014-01-24 08:29:20 +0100

    contribute a second Forge console from the F2 plugins

---
+++ plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/console/ForgeTwoConsole.java
@@ -0,0 +1,11 @@
+package org.jboss.tools.forge.ui.ext.console;
+
+import org.jboss.tools.forge.ui.console.ForgeConsole;
+
+public class ForgeTwoConsole implements ForgeConsole {
+	
+	public ForgeTwoConsole() {
+		System.out.println("creating ForgeTwoConsole");
+	}
+
+}
