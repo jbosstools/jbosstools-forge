411b2f612ecccf8610b0d4f948907c72a629e42c
Koen Aers <koen.aers@gmail.com>
2014-02-12 14:06:42 +0100

    enable GoToAction in combined Forge Console View

--- plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/actions/f1/GoToAction.java
+++ plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/actions/f1/GoToAction.java
@@ -2,23 +2,48 @@ package org.jboss.tools.forge.ui.actions.f1;
 
 import java.net.URL;
 
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.rse.subsystems.files.core.subsystems.IRemoteFile;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.ISelectionService;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+import org.jboss.tools.forge.core.preferences.ForgeRuntimesPreferences;
+import org.jboss.tools.forge.core.process.ForgeRuntime;
 import org.jboss.tools.forge.ui.ForgeUIPlugin;
 import org.jboss.tools.forge.ui.util.ForgeHelper;
 
-public class GoToAction extends Action {
-
+public class GoToAction extends Action implements ISelectionListener {
+	
+	private ISelection selection;
+	
 	public GoToAction() {
 		super();
 		setImageDescriptor(createImageDescriptor());
+		getSelectionService().addPostSelectionListener(this);
 	}
 
 	@Override
 	public void run() {
-		System.out.println("clicked goto");
+		goToSelection();
 	}
 	
+	@Override
+	protected void finalize() {
+		getSelectionService().removeSelectionListener(this);
+	}
+	
+	@Override
+	public void selectionChanged(IWorkbenchPart part, ISelection newSelection) {
+		selection = newSelection;
+	}
+
 	public boolean isEnabled() {
 		return ForgeHelper.isForgeRunning();
 	}
@@ -28,4 +53,40 @@ public class GoToAction extends Action {
 		return ImageDescriptor.createFromURL(url);
 	}
 
+	public boolean goToSelection() {
+		if (selection instanceof IStructuredSelection) {
+		    IStructuredSelection ss = (IStructuredSelection)selection;
+		    Object first = ss.getFirstElement();
+		    if (first instanceof IResource) {
+		    	goToPath(((IResource)first).getLocation().toOSString());
+		    } else if (first instanceof IJavaElement) {
+		    	try {
+		    		IResource resource = ((IJavaElement)first).getCorrespondingResource();
+		    		if (resource == null) return false;
+					goToPath(resource.getLocation().toOSString());
+				} catch (JavaModelException e) {
+					ForgeUIPlugin.log(e);
+					return false;
+				}
+		    } else if (first instanceof IRemoteFile) {
+		    	goToPath(((IRemoteFile)first).getAbsolutePath());
+		    }
+		    return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private void goToPath(String str) {
+		if (str.indexOf(' ') != -1) {
+			str = '\"' + str + '\"';
+		}
+		ForgeRuntime runtime = ForgeRuntimesPreferences.INSTANCE.getDefaultRuntime();
+		runtime.sendInput("pick-up " + str + "\n");
+	}
+	
+	private ISelectionService getSelectionService() {
+		return PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService();
+	}
+	
 }
