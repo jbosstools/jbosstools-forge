db179a4b19c7c1eb2371a56c9b26a54e337af7f9
Koen Aers <koen.aers@gmail.com>
2014-03-08 12:28:45 +0100

    remove unused connect(AeshConsole) and disconnect() methods as well as unused stream listeners and filters

--- plugins/org.jboss.tools.aesh.ui/src/org/jboss/tools/aesh/ui/document/DelegateDocument.java
+++ plugins/org.jboss.tools.aesh.ui/src/org/jboss/tools/aesh/ui/document/DelegateDocument.java
@@ -3,18 +3,11 @@ package org.jboss.tools.aesh.ui.document;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
 import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.TextStyle;
-import org.eclipse.swt.widgets.Display;
-import org.jboss.tools.aesh.core.ansi.ControlSequence;
-import org.jboss.tools.aesh.core.ansi.ControlSequenceFilter;
-import org.jboss.tools.aesh.core.ansi.ControlSequenceHandler;
-import org.jboss.tools.aesh.core.console.AeshConsole;
-import org.jboss.tools.aesh.core.io.StreamListener;
 import org.jboss.tools.aesh.ui.fonts.FontManager;
 
 public class DelegateDocument extends Document {
@@ -23,11 +16,7 @@ public class DelegateDocument extends Document {
 		void cursorMoved();
 	}
 	
-	private StreamListener stdOutListener, stdErrListener;
-	private ControlSequenceFilter commandSequenceFilter;
-	private ControlSequenceHandler ansiCommandSequenceHandler;
 	private int cursorOffset = 0;
-	private AeshConsole console;
 	private Set<CursorListener> cursorListeners = new HashSet<CursorListener>();
 	private DelegateStyleRange currentStyleRange;
 	private DelegatingDocument proxy;
@@ -37,43 +26,6 @@ public class DelegateDocument extends Document {
 	public DelegateDocument() {
 		proxy = new DelegatingDocument(this);
 		currentStyleRange = getDefaultStyleRange();
-		stdOutListener = new StreamListener() {			
-			@Override
-			public void outputAvailable(final String output) {
-				Display.getDefault().syncExec(new Runnable() {
-					@Override
-					public void run() {
-						handleOutputAvailable(output);
-					}				
-				});
-			}
-		};
-		ansiCommandSequenceHandler = new ControlSequenceHandler() {			
-			@Override
-			public void handle(final ControlSequence controlSequence) {
-				Display.getDefault().syncExec(new Runnable() {
-					@Override
-					public void run() {
-						controlSequence.handle(proxy);
-					}					
-				});
-			}
-		};
-		commandSequenceFilter = 
-				new ControlSequenceFilter(
-						stdOutListener, 
-						ansiCommandSequenceHandler);
-		stdErrListener = new StreamListener() {		
-			@Override
-			public void outputAvailable(final String output) {
-				Display.getDefault().syncExec(new Runnable() {
-					@Override
-					public void run() {
-						handleOutputAvailable(output);
-					}				
-				});
-			}
-		};
 	}
 	
 	void saveCursor() {
@@ -94,36 +46,6 @@ public class DelegateDocument extends Document {
 		cursorOffset = newOffset;
 		for (CursorListener listener : cursorListeners) {
 			listener.cursorMoved();
-		}
-	}
-
-	private void handleOutputAvailable(String output) {
-		try {
-			output.replaceAll("\r", "");
-			if (currentStyleRange != null) {
-				int increase = getCursorOffset() -getLength() + output.length();
-				currentStyleRange.length += increase;
-			}
-			replace(getCursorOffset(), getLength() - getCursorOffset(), output);
-			moveCursorTo(getCursorOffset() + output.length());
-		} catch (BadLocationException e) {
-        	e.printStackTrace();							
-		}
-	}
-	
-	public void connect(AeshConsole aeshConsole) {
-		if (console == null) {
-			console = aeshConsole;
-			console.addStdOutListener(commandSequenceFilter);
-			console.addStdErrListener(stdErrListener);
-		}
-	}
-	
-	public void disconnect() {
-		if (console != null) {
-			console.removeStdOutListener(commandSequenceFilter);
-			console.removeStdErrListener(stdErrListener);
-			console = null;
 		}
 	}
 
