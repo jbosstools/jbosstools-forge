2a086bc41bb5e7d24e8db839c4e1b2bf6b92337e
Koen Aers <koen.aers@gmail.com>
2014-03-12 11:30:56 +0100

    add test for CursorHorizontalAbsolute

---
+++ tests/org.jboss.tools.aesh.core.test/src/org/jboss/tools/aesh/core/internal/ansi/CursorHorizontalAbsoluteTest.java
@@ -0,0 +1,33 @@
+package org.jboss.tools.aesh.core.internal.ansi;
+
+import org.jboss.tools.aesh.core.document.Document;
+import org.jboss.tools.aesh.core.test.util.TestDocument;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CursorHorizontalAbsoluteTest {
+	
+	private int testOffset = 0;
+	
+	private Document testDocument = new TestDocument() {
+		@Override public int getLineOffset(int line) { return line * 80; }
+		@Override public int getLineOfOffset(int offset) { return offset / 80; }
+		@Override public int getCursorOffset() { return testOffset; }
+		@Override public void moveCursorTo(int offset) { testOffset = offset; }
+	};
+	
+	@Test
+	public void testGetType() {
+		CursorHorizontalAbsolute cursorHorizontalAbsolute = new CursorHorizontalAbsolute("17");
+		Assert.assertEquals(CommandType.CURSOR_HORIZONTAL_ABSOLUTE, cursorHorizontalAbsolute.getType());
+	}
+	
+	@Test
+	public void testHandle() {
+		CursorHorizontalAbsolute cursorHorizontalAbsolute = new CursorHorizontalAbsolute("17");
+		testOffset = 833;
+		cursorHorizontalAbsolute.handle(testDocument);
+		Assert.assertEquals(817, testOffset);
+	}
+
+}
