64b670bcc8e93e2f620dd7a5bd687a83686ef766
Koen Aers <koen.aers@gmail.com>
2014-03-12 14:37:00 +0100

    add test for EraseInLine

---
+++ tests/org.jboss.tools.aesh.core.test/src/org/jboss/tools/aesh/core/internal/ansi/EraseInLineTest.java
@@ -0,0 +1,49 @@
+package org.jboss.tools.aesh.core.internal.ansi;
+
+import org.jboss.tools.aesh.core.document.Document;
+import org.jboss.tools.aesh.core.test.util.TestDocument;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EraseInLineTest {
+	
+	private int cursorOffset = 35;
+	private int length = 45;
+	private boolean defaultStyleSet = false;
+	
+	private int replacedPos = 0;
+	private int replacedLength = 0;
+	private String replacedText = null;
+	
+	private Document testDocument = new TestDocument() {
+		@Override public int getCursorOffset() { return cursorOffset; }
+		@Override public int getLength() { return length; }
+		@Override public void setDefaultStyle() { defaultStyleSet = true; }
+		@Override public void replace(int pos, int length, String text) {
+			replacedPos = pos;
+			replacedLength = length;
+			replacedText = text;
+		}
+	};
+	
+	@Test
+	public void testGetType() {
+		EraseInLine eraseInLine = new EraseInLine(null);
+		Assert.assertEquals(CommandType.ERASE_IN_LINE, eraseInLine.getType());
+	}
+	
+	@Test
+	public void testHandle() {
+		EraseInLine eraseInLine = new EraseInLine(null);
+		Assert.assertEquals(0, replacedPos);
+		Assert.assertEquals(0, replacedLength);
+		Assert.assertNull(replacedText);
+		Assert.assertFalse(defaultStyleSet);
+		eraseInLine.handle(testDocument);
+		Assert.assertEquals(35, replacedPos);
+		Assert.assertEquals(10, replacedLength);
+		Assert.assertEquals("", replacedText);
+		Assert.assertTrue(defaultStyleSet);
+	}
+
+}
