d27ca18ab5ebff4a19d395375754f62a98050022
Koen Aers <koen.aers@gmail.com>
2014-04-01 18:38:08 +0200

    JBIDE-16479: initial implementation of post execution selection

--- plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/cli/CommandExecutionListenerImpl.java
+++ plugins/org.jboss.tools.forge.ui.ext/src/org/jboss/tools/forge/ui/ext/cli/CommandExecutionListenerImpl.java
@@ -1,9 +1,37 @@
 package org.jboss.tools.forge.ui.ext.cli;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileInfo;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TreePath;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.rse.ui.view.IRSEViewPart;
+import org.eclipse.rse.ui.view.ISystemViewElementAdapter;
+import org.eclipse.rse.ui.view.SystemAdapterHelpers;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.navigator.CommonNavigator;
+import org.eclipse.ui.part.ISetSelectionTarget;
 import org.jboss.forge.addon.projects.Project;
 import org.jboss.forge.addon.projects.ProjectListener;
 import org.jboss.forge.addon.projects.facets.MetadataFacet;
@@ -13,6 +41,7 @@ import org.jboss.forge.addon.ui.command.UICommand;
 import org.jboss.forge.addon.ui.context.UIExecutionContext;
 import org.jboss.forge.addon.ui.context.UISelection;
 import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.tools.forge.ui.ext.ForgeUIPlugin;
 import org.jboss.tools.forge.ui.ext.importer.ProjectImporter;
 
 public class CommandExecutionListenerImpl 
@@ -24,36 +53,51 @@ implements ProjectListener {
 	@Override
 	public void postCommandExecuted(
 			UICommand command, 
-			UIExecutionContext uiExecutionContext,
+			final UIExecutionContext uiExecutionContext,
 			Result result) {
-		if (!projects.isEmpty()) {
-			importProjects();
-			projects.clear();
-		}
-		UISelection<?> selection = uiExecutionContext.getUIContext().getInitialSelection();
-		Iterator<?> iterator = selection.iterator();
-		while (iterator.hasNext()) {
-			Object object = iterator.next();
-			if (object instanceof Resource<?>) {
-				refresh((Resource<?>)object);
-			}
-		}
-		Object object = uiExecutionContext.getUIContext().getSelection();
-		if (object != null) {
-			if (object instanceof Resource<?>) {
-				select((Resource<?>)object);
-			}
-		}
+		Display.getDefault().syncExec(new Runnable() {
+			@Override
+			public void run() {
+				if (!projects.isEmpty()) {
+					importProjects();
+					projects.clear();
+				}
+				UISelection<?> selection = uiExecutionContext.getUIContext().getInitialSelection();
+				Iterator<?> iterator = selection.iterator();
+				while (iterator.hasNext()) {
+					Object object = iterator.next();
+					if (object instanceof Resource<?>) {
+						refresh((Resource<?>)object);
+					}
+				}
+				Object object = uiExecutionContext.getUIContext().getSelection();
+				if (object != null) {
+					if (object instanceof Resource<?>) {
+						selectResource((Resource<?>)object);
+					}
+				}
+			}			
+		});
 	}
 	
 	private void refresh(Resource<?> resource) {
 		System.out.println("refreshing initial selection: " + resource.getFullyQualifiedName());
 	}
 	
-	private void select(Resource<?> resource) {
+	private void selectResource(Resource<?> resource) {
+		Object object = resource.getUnderlyingResourceObject();
 		System.out.println("highlighting final selection: " + resource.getFullyQualifiedName());
+		System.out.println("the object is of class : " + object.getClass().getName());
+		if (object instanceof File) {
+			selectFile((File)object);
+		}
 	}
 	
+	@Override
+	public void projectCreated(Project project) {
+		projects.add(project);
+	}
+
 	private void importProjects() {
 		for (Project project : projects) {
 			Resource<?> projectRoot = project.getRoot();
@@ -68,9 +112,143 @@ implements ProjectListener {
 		}
 	}
 
-	@Override
-	public void projectCreated(Project project) {
-		projects.add(project);
+	private void selectFile(File file) {
+		try {
+			IPath path = new Path(file.getCanonicalPath());
+			IFileStore fileStore = EFS.getLocalFileSystem().getStore(path);
+			IFileInfo fileInfo = fileStore.fetchInfo();
+			if (!fileInfo.exists()) return;
+			if (fileInfo.isDirectory()) {
+				IContainer container = ResourcesPlugin.getWorkspace().getRoot().getContainerForLocation(path);
+				if (container != null) {
+					expandWorkspaceDirectory(container);
+				} else {
+					expandSystemDirectory(fileStore);
+				}
+			} else {
+				openFileInEditor(fileStore);
+			}
+		} catch (IOException e) {
+			ForgeUIPlugin.log(e);
+		}
+	}
+	
+	private void expandWorkspaceDirectory(IContainer container) {
+		IWorkbenchPage workbenchPage = getActiveWorkbenchPage();
+		if (workbenchPage != null) {
+			IViewPart projectExplorer = workbenchPage.findView("org.eclipse.ui.navigator.ProjectExplorer");
+			if (projectExplorer != null && projectExplorer instanceof CommonNavigator) {
+				expandInProjectExplorer((CommonNavigator)projectExplorer, container);
+			} 
+			IViewPart packageExplorer = workbenchPage.findView("org.eclipse.jdt.ui.PackageExplorer"); 
+			if (packageExplorer != null) {
+				expandInPackageExplorer(packageExplorer, container);
+			}
+		}
+	}
+	
+	private IWorkbenchPage getActiveWorkbenchPage() {
+		IWorkbenchPage result = null;
+		IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		if (workbenchWindow != null) {
+			result = workbenchWindow.getActivePage();
+		}
+		if (result != null) {
+			
+		}
+		return result;		
 	}
 
+	private void expandInProjectExplorer(CommonNavigator projectExplorer, IContainer container) {
+		projectExplorer.selectReveal(new StructuredSelection(container));
+		TreeViewer treeViewer = projectExplorer.getCommonViewer();
+		treeViewer.expandToLevel(container, 1);
+	}
+	
+	private void expandInPackageExplorer(IViewPart packageExplorer, IContainer container) {
+		if (packageExplorer instanceof ISetSelectionTarget) {
+			((ISetSelectionTarget)packageExplorer).selectReveal(new StructuredSelection(container));
+		}
+		Object treeViewer = packageExplorer.getAdapter(ISelectionProvider.class);
+		if (treeViewer != null && treeViewer instanceof TreeViewer) {
+			((TreeViewer)treeViewer).expandToLevel(JavaCore.create(container), 1);
+		}
+	}
+	
+	private void expandSystemDirectory(IFileStore fileStore) {
+		IWorkbenchPage workbenchPage = getActiveWorkbenchPage();
+		IViewPart remoteSystemView = workbenchPage.findView("org.eclipse.rse.ui.view.systemView");
+		if (remoteSystemView != null) {
+			expandInRemoteSystemView(remoteSystemView, fileStore);
+		}
+	}
+	
+	private void expandInRemoteSystemView(
+			IViewPart remoteSystemView, 
+			IFileStore fileStore) {
+		Viewer viewer = getViewer(remoteSystemView);
+		Object input = viewer.getInput();
+		ArrayList<String> names = createSegmentNames(fileStore);
+		ArrayList<Object> treeSegments = new ArrayList<Object>();
+		for (String name : names) {
+			if (input != null && input instanceof IAdaptable) {
+				ISystemViewElementAdapter adapter = SystemAdapterHelpers.getViewAdapter(input);
+				if (adapter != null) {
+					for (Object object : adapter.getChildren((IAdaptable)input, null)) {
+						if (object instanceof IAdaptable) {
+							adapter = SystemAdapterHelpers.getViewAdapter(object);
+							if (adapter != null && name.equals(adapter.getText(object))) {
+								input = object;
+								treeSegments.add(input);
+								break;
+							}
+						}
+					}
+				}
+			} else {
+				treeSegments.clear();
+				break;
+			}
+		}
+		TreePath treePath = new TreePath(treeSegments.toArray());
+		viewer.setSelection(new StructuredSelection(treePath));
+		if (viewer instanceof TreeViewer) {
+			((TreeViewer)viewer).expandToLevel(treePath, 1);
+			
+		}
+	}
+
+	private Viewer getViewer(IViewPart remoteSystemView) {
+		if (remoteSystemView instanceof IRSEViewPart) {
+			return ((IRSEViewPart)remoteSystemView).getRSEViewer();
+		} else {
+			return null;
+		}
+	}
+	
+	private ArrayList<String> createSegmentNames(IFileStore fileStore) {
+		ArrayList<String> result = new ArrayList<String>();
+		while (fileStore.getParent() != null) {
+			result.add(0, fileStore.getName());
+			fileStore = fileStore.getParent();
+		}
+		result.add(0, "/");
+		result.add(0, "Root");
+		result.add(0, "Local Files");
+		result.add(0, "Local");
+		return result;
+	}
+	
+	private void openFileInEditor(IFileStore fileStore) {
+		try {
+			IWorkbenchPage workbenchPage = getActiveWorkbenchPage();
+			if (workbenchPage != null) {
+				IDE.openEditorOnFileStore(workbenchPage, fileStore);
+			}
+		} catch (PartInitException e) {
+			ForgeUIPlugin.log(e);
+		}
+	}
+	
+	
 }
