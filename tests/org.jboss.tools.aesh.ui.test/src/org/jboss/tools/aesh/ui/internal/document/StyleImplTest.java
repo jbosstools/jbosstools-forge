eca23b25cb55a3473b0e639d68f82b779a115566
Koen Aers <koen.aers@gmail.com>
2014-04-13 12:51:08 +0200

    add test for getStyleRange()

--- tests/org.jboss.tools.aesh.ui.test/src/org/jboss/tools/aesh/ui/internal/document/StyleImplTest.java
+++ tests/org.jboss.tools.aesh.ui.test/src/org/jboss/tools/aesh/ui/internal/document/StyleImplTest.java
@@ -1,11 +1,28 @@
 package org.jboss.tools.aesh.ui.internal.document;
 
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.widgets.Display;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StyleImplTest {
+	
+	private StyleRange testStyleRange;
+	private StyleImpl testStyleImpl;
+	
+	@Before
+	public void setup() {
+		testStyleRange = 
+				new StyleRange(
+						99, 
+						999, 
+						Display.getDefault().getSystemColor(SWT.COLOR_DARK_GRAY), 
+						Display.getDefault().getSystemColor(SWT.COLOR_GRAY));
+		testStyleImpl = new StyleImpl(testStyleRange);
+	}
 	
 	@Test
 	public void testConstructor() {
@@ -13,5 +30,11 @@ public class StyleImplTest {
 		StyleImpl styleImpl = new StyleImpl(styleRange);
 		Assert.assertEquals(styleRange, styleImpl.styleRange);
 	}
+	
+	@Test
+	public void testGetStyleRange() {
+		Assert.assertEquals(testStyleRange, testStyleImpl.getStyleRange());
+	}
+	
 
 }
