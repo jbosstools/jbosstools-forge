b9978acbd39e9a0ccab00bb9164a9a22de25ea11
Koen Aers <koen.aers@gmail.com>
2014-04-28 16:46:33 +0200

    JBIDE-16928: Forge2 runtime should be selected by default in Forge Console view

--- plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/internal/console/ForgeConsoleManager.java
+++ plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/internal/console/ForgeConsoleManager.java
@@ -7,6 +7,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.Platform;
+import org.jboss.tools.forge.core.runtime.ForgeRuntime;
 import org.jboss.tools.forge.ui.internal.ForgeUIPlugin;
 
 public class ForgeConsoleManager {
@@ -24,7 +25,9 @@ public class ForgeConsoleManager {
         				"org.jboss.tools.forge.ui.consoles");
         for (IConfigurationElement element : extensionPoint.getConfigurationElements()) {
         	try {
-				consoles.add((ForgeConsole)element.createExecutableExtension("class"));
+        		ForgeConsole forgeConsole = (ForgeConsole)element.createExecutableExtension("class");
+        		int index = calculateIndex(forgeConsole);
+				consoles.add(index, forgeConsole);
 			} catch (CoreException e) {
 				ForgeUIPlugin.log(e);
 			}
@@ -34,6 +37,29 @@ public class ForgeConsoleManager {
 	
 	public ForgeConsole[] getConsoles() {
 		return consoles.toArray(new ForgeConsole[consoles.size()]);
+	}
+	
+	private int calculateIndex(ForgeConsole forgeConsole) {
+		ForgeRuntime runtime = forgeConsole.getRuntime();
+		String newVersion = runtime.getVersion();
+		for (int i = 0; i < consoles.size(); i++) {
+			String version = consoles.get(i).getRuntime().getVersion();
+			if (isNewer(newVersion, version)) {
+				return i;
+			}
+		}
+		return consoles.size();
+	}
+	
+	private boolean isNewer(String first, String second) {
+		String[] firstValues = first.split("\\.|-");
+		String[] secondValues = second.split("\\.|-");
+		for (int i = 0; i < 2; i++) {
+			if (Integer.valueOf(firstValues[i]) > Integer.valueOf(secondValues[i])) {
+				return true;
+			}
+		}
+		return "Final".equals(firstValues[3]);
 	}
 
 }
