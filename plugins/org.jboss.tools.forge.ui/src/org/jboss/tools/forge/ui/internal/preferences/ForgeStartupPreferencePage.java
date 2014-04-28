e5bc86b73b52bb31a565e4ffa9e6f3d5102b1516
Koen Aers <koen.aers@gmail.com>
2014-04-28 18:53:51 +0200

    add addon repository config preference to main preference page

--- plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/internal/preferences/ForgeStartupPreferencePage.java
+++ plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/internal/preferences/ForgeStartupPreferencePage.java
@@ -2,11 +2,14 @@ package org.jboss.tools.forge.ui.internal.preferences;
 
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
@@ -19,6 +22,7 @@ public class ForgeStartupPreferencePage extends PreferencePage implements
 	private Button startupButton;
 	private Button startInDebugButton;
 	private Text vmArgsText;
+	private Text addonDirText;
 	  
 	protected Control createContents(Composite parent) {
 		noDefaultAndApplyButton();
@@ -26,6 +30,7 @@ public class ForgeStartupPreferencePage extends PreferencePage implements
 		createStartupButton(clientArea);
 		createStartInDebugButton(clientArea);
 		createVmArgsText(clientArea);
+		createAddonDirText(parent);
 		return null;
 	}
 	
@@ -56,6 +61,40 @@ public class ForgeStartupPreferencePage extends PreferencePage implements
 		vmArgsText.setText(vmArgs == null ? "" : vmArgs);
 	}
 	
+	private void createAddonDirText(Composite parent) {
+		Label addonDirLabel = new Label(parent, SWT.NONE);
+		addonDirLabel.setText("Forge Addon Repository Location: ");
+
+		Composite container = new Composite(parent, SWT.NULL);
+		container.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+		GridLayout layout = new GridLayout();
+		container.setLayout(layout);
+		layout.numColumns = 2;
+		layout.verticalSpacing = 9;
+		layout.marginWidth = 0;
+		layout.marginHeight = 0;
+
+		addonDirText = new Text(container, SWT.BORDER);
+		addonDirText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		addonDirText.setText(ForgeCorePreferences.INSTANCE.getAddonDir());
+		Button button = new Button(container, SWT.PUSH);
+		button.setText("Browse...");
+		button.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				DirectoryDialog dialog = new DirectoryDialog(getShell(),
+						SWT.OPEN);
+				dialog.setText("Select a directory");
+				dialog.setFilterPath(addonDirText.getText());
+				String selectedPath = dialog.open();
+				if (selectedPath != null) {
+					addonDirText.setText(selectedPath);
+				}
+			}
+		});
+	}
+
 	private Composite createClientArea(Composite parent) {
 		Composite clientArea = new Composite(parent, SWT.NONE);
 		GridLayout layout = new GridLayout();
@@ -77,6 +116,7 @@ public class ForgeStartupPreferencePage extends PreferencePage implements
 		ForgeCorePreferences.INSTANCE.setStartup(startupButton.getSelection());
 		ForgeCorePreferences.INSTANCE.setStartInDebug(startInDebugButton.getSelection());
 		ForgeCorePreferences.INSTANCE.setVmArgs(getVmArgsText());
+		ForgeCorePreferences.INSTANCE.setAddonDir(addonDirText.getText());
 		return true;
 	}
 }
