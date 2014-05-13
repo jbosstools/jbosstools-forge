f3c1ad0129c20dcf7dac5fb9c8341ace64a94287
George Gastaldi <gegastaldi@gmail.com>
2014-05-13 16:16:30 -0300

    Commenting out RuntimeException thrown since it crashes the console

--- plugins/org.jboss.tools.aesh.core/src/org/jboss/tools/aesh/core/internal/ansi/AbstractCommand.java
+++ plugins/org.jboss.tools.aesh.core/src/org/jboss/tools/aesh/core/internal/ansi/AbstractCommand.java
@@ -3,13 +3,13 @@ package org.jboss.tools.aesh.core.internal.ansi;
 import org.jboss.tools.aesh.core.document.Document;
 
 public abstract class AbstractCommand implements Command {
-	
+
 	public final static String NOT_IMPLEMENTED = "not implemented!";
-	
+
 	public abstract CommandType getType();
-	
+
 	public void handle(Document document) {
-		throw new RuntimeException(NOT_IMPLEMENTED);
+		// throw new RuntimeException(NOT_IMPLEMENTED);
 	}
 
 }
