fe434315b8a95439d83dcc52fc00a9037ec7d47e
Koen Aers <koen.aers@gmail.com>
2014-05-25 07:46:12 +0200

    create class AbstractForgeConsole

---
+++ plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/internal/console/AbstractForgeConsole.java
@@ -0,0 +1,20 @@
+package org.jboss.tools.forge.ui.internal.console;
+
+import java.beans.PropertyChangeListener;
+
+import org.jboss.tools.forge.core.runtime.ForgeRuntime;
+
+public abstract class AbstractForgeConsole 
+implements ForgeConsole, PropertyChangeListener {
+	
+	private ForgeRuntime runtime;
+	
+	public AbstractForgeConsole(ForgeRuntime runtime) {
+		this.runtime = runtime;
+	}
+	
+	public ForgeRuntime getRuntime() {
+		return runtime;
+	}
+
+}
