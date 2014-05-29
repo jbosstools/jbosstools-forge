f778cb524b72b1eceda726204f40c862b711622c
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2014-05-29 18:33:30 -0400

    Improve docs.

--- plugins/org.jboss.tools.forge.core/src/org/jboss/tools/forge/core/furnace/repository/IFurnaceRepositoryProvider.java
+++ plugins/org.jboss.tools.forge.core/src/org/jboss/tools/forge/core/furnace/repository/IFurnaceRepositoryProvider.java
@@ -4,19 +4,20 @@ import java.util.List;
 
 /**
  * <p>
- * Interface implemented by users of the <code>org.jboss.tools.forge.core.furnaceRepository</code>
- * extension point.
+ * Interface implemented by users of the <code>org.jboss.tools.forge.core.furnaceRepository</code> extension point.
  * </p>
  */
-public interface IFurnaceRepositoryProvider {
-	
-	/**
-	 * @return list of Furnace add on repositories to add to Furnace
-	 */
-	public List<IFurnaceRepository> getRepositories();
+public interface IFurnaceRepositoryProvider
+{
 
-	/**
-	 * @return the {@link ClassLoader} instance on which Furnace should depend for {@link Class} type proxying.
-	 */
+   /**
+    * @return list of Furnace add on repositories to add to Furnace
+    */
+   public List<IFurnaceRepository> getRepositories();
+
+   /**
+    * @return the {@link ClassLoader} instance on which Furnace should depend for {@link Class} type proxying. Typically
+    *         this will be the {@link ClassLoader} that loaded your implementation of this interface.
+    */
    public ClassLoader getClassLoader();
 }
