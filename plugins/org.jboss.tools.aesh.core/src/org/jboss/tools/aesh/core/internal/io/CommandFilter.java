42acc8ba738a90c574742eb7b8f107e099e515b3
Koen Aers <koen.aers@gmail.com>
2014-07-01 16:31:45 +0200

    remove unneeded method in CommandFilter

--- plugins/org.jboss.tools.aesh.core/src/org/jboss/tools/aesh/core/internal/io/CommandFilter.java
+++ plugins/org.jboss.tools.aesh.core/src/org/jboss/tools/aesh/core/internal/io/CommandFilter.java
@@ -8,9 +8,6 @@ public class CommandFilter implements AeshOutputFilter {
 	
 	private static final String ESCAPE_SEQUENCE = new String(new byte[] { 27, '[' });
 
-	private StringBuffer escapeSequence = new StringBuffer();
-	private StringBuffer targetBuffer = new StringBuffer();
-	
 	private CommandFactory commandFactory = DefaultCommandFactory.INSTANCE;
 	
 	private AeshOutputHandler handler = null;
@@ -40,39 +37,6 @@ public class CommandFilter implements AeshOutputFilter {
 			}
 		}
 		handler.handleOutput(output.substring(index));
-	}
-	
-	public void filterOutput2(String output) {
-		for (int i = 0; i < output.length(); i++) {
-			charAppended(output.charAt(i));
-		}
-		if (targetBuffer.length() > 0) {
-			String targetString = targetBuffer.toString();
-			targetBuffer.setLength(0);
-			handler.handleOutput(targetString);
-		}
-	}
-	
-	private void charAppended(char c) {
-		if (c == 27 && escapeSequence.length() == 0) {
-			if (targetBuffer.length() > 0) {
-				String targetString = targetBuffer.toString();
-				targetBuffer.setLength(0);
-				handler.handleOutput(targetString);
-			}
-			escapeSequence.append(c);
-		} else if (c == '[' && escapeSequence.length() == 1) {
-			escapeSequence.append(c);
-		} else if (escapeSequence.length() > 1) {
-			escapeSequence.append(c);
-			Command controlSequence = commandFactory.create(escapeSequence.toString());
-			if (controlSequence != null) {
-				escapeSequence.setLength(0);
-				handler.handleCommand(controlSequence);
-			}
-		} else {
-			targetBuffer.append(c);
-		}
 	}
 	
 	void setCommandFactory(CommandFactory commandFactory) {
