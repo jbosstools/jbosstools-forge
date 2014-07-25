4c4690760e9807b3a604ffbc8aeeb895480a90d4
George Gastaldi <gegastaldi@gmail.com>
2014-07-25 12:50:31 -0300

    JBIDE-17890: Support for #/projectName in Forge 2 console

--- plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/internal/cli/WorkspaceCdTokenHandler.java
+++ plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/internal/cli/WorkspaceCdTokenHandler.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
 import org.jboss.forge.addon.resource.Resource;
 import org.jboss.forge.addon.resource.ResourceFactory;
 import org.jboss.forge.addon.shell.spi.command.CdTokenHandler;
@@ -14,26 +16,34 @@ import org.jboss.forge.addon.ui.context.UIContext;
  * Handles the '#' workspace shortcut character in JBossTools Forge CLI
  * 
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
  */
-public class WorkspaceCdTokenHandler implements CdTokenHandler
-{
-   private ResourceFactory resourceFactory;
+public class WorkspaceCdTokenHandler implements CdTokenHandler {
+	private ResourceFactory resourceFactory;
 
-   public WorkspaceCdTokenHandler(ResourceFactory resourceFactory)
-   {
-      this.resourceFactory = resourceFactory;
-   }
+	public WorkspaceCdTokenHandler(ResourceFactory resourceFactory) {
+		this.resourceFactory = resourceFactory;
+	}
 
-   @Override
-   public List<Resource<?>> getNewCurrentResources(UIContext context, String token)
-   {
-      List<Resource<?>> result = new ArrayList<>();
-      if (token.startsWith("#"))
-      {
-         File file = ResourcesPlugin.getWorkspace().getRoot().getLocation().makeAbsolute().toFile();
-         Resource<File> resource = resourceFactory.create(new File(file, token.replaceFirst("#", "")));
-         result.add(resource);
-      }
-      return result;
-   }
+	@Override
+	public List<Resource<?>> getNewCurrentResources(UIContext context,
+			String token) {
+		List<Resource<?>> result = new ArrayList<>();
+		if (token.startsWith("#")) {
+			IPath location;
+			String projectName = token.replaceFirst("#/?", "");
+			IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+			if (projectName.isEmpty()) {
+				location = root.getLocation();
+			} else {
+				location = root.getProject(projectName).getLocation();
+			}
+			if (location != null) {
+				File file = location.makeAbsolute().toFile();
+				Resource<File> resource = resourceFactory.create(file);
+				result.add(resource);
+			}
+		}
+		return result;
+	}
 }
