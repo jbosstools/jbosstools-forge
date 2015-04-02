2670bd8ef0f1ab5dd0393d862dc6cb8cf64c4d77
George Gastaldi <gegastaldi@gmail.com>
2015-04-02 20:10:18 -0300

    JBIDE-19550: Export missing packages for external plugins

--- plugins/org.jboss.tools.forge.core/src/org/jboss/tools/forge/core/internal/furnace/CompositeFurnaceClassLoader.java
+++ plugins/org.jboss.tools.forge.core/src/org/jboss/tools/forge/core/internal/furnace/CompositeFurnaceClassLoader.java
@@ -6,42 +6,34 @@
  */
 package org.jboss.tools.forge.core.internal.furnace;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
-public class CompositeFurnaceClassLoader extends ClassLoader
-{
-   private final List<ClassLoader> loaders = Collections.synchronizedList(new ArrayList<ClassLoader>());
+public class CompositeFurnaceClassLoader extends ClassLoader {
+	private final Set<ClassLoader> loaders = new LinkedHashSet<>();
 
-   public CompositeFurnaceClassLoader(List<ClassLoader> loaders)
-   {
-      super(null);
-      this.loaders.addAll(loaders);
-      this.loaders.remove(this);
-   }
+	public CompositeFurnaceClassLoader(Collection<ClassLoader> loaders) {
+		super(null);
+		this.loaders.addAll(loaders);
+		this.loaders.remove(this);
+	}
 
-   @Override
-   public Class<?> loadClass(String name) throws ClassNotFoundException
-   {
-      for (ClassLoader classLoader : loaders)
-      {
-         try
-         {
-            return classLoader.loadClass(name);
-         }
-         catch (ClassNotFoundException notFound)
-         {
-            // oh well
-         }
-      }
+	@Override
+	public Class<?> loadClass(String name) throws ClassNotFoundException {
+		for (ClassLoader classLoader : loaders) {
+			try {
+				return classLoader.loadClass(name);
+			} catch (ClassNotFoundException notFound) {
+				// oh well
+			}
+		}
 
-      throw new ClassNotFoundException(name);
-   }
+		throw new ClassNotFoundException(name);
+	}
 
-   @Override
-   public String toString()
-   {
-      return loaders.toString();
-   }
+	@Override
+	public String toString() {
+		return loaders.toString();
+	}
 }
