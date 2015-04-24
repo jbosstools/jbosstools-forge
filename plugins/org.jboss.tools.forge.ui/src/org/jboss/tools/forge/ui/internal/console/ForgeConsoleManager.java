a1fa2a685ae9182ccf3fa9c3c310c339bd66271c
George Gastaldi <gegastaldi@gmail.com>
2015-04-24 15:14:11 -0300

    Delete Forge 1.x remains

--- plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/internal/console/ForgeConsoleManager.java
+++ plugins/org.jboss.tools.forge.ui/src/org/jboss/tools/forge/ui/internal/console/ForgeConsoleManager.java
@@ -14,35 +14,31 @@ import org.jboss.tools.forge.core.runtime.ForgeRuntime;
 
 public enum ForgeConsoleManager {
 	INSTANCE;
-	
+
 	private List<ForgeConsole> consoles = new ArrayList<ForgeConsole>();
-	
+
 	private ForgeConsoleManager() {
 		createConsoles();
 	}
-	
+
 	private void createConsoles() {
 		consoles = new ArrayList<ForgeConsole>();
 		for (ForgeRuntime runtime : ForgeCorePreferences.INSTANCE.getRuntimes()) {
-			ForgeConsole console = null;
-			if (runtime.getVersion().startsWith("1.")) {
-				console = new org.jboss.tools.forge.ui.internal.console.F1Console(runtime);
-			} else {
-				console = new org.jboss.tools.forge.ui.internal.console.F2Console(runtime);
-			}
+			ForgeConsole console = new org.jboss.tools.forge.ui.internal.console.F2Console(
+					runtime);
 			int index = calculateIndex(console);
 			consoles.add(index, console);
 		}
 	}
-	
+
 	public ForgeConsole[] getConsoles() {
 		return consoles.toArray(new ForgeConsole[consoles.size()]);
 	}
-	
+
 	public ForgeConsole getDefaultConsole() {
 		return getConsole(ForgeCorePreferences.INSTANCE.getDefaultRuntime());
 	}
-	
+
 	public ForgeConsole getConsole(ForgeRuntime runtime) {
 		for (ForgeConsole console : getConsoles()) {
 			if (console.getRuntime() == runtime) {
@@ -51,7 +47,7 @@ public enum ForgeConsoleManager {
 		}
 		return null;
 	}
-	
+
 	private int calculateIndex(ForgeConsole forgeConsole) {
 		ForgeRuntime runtime = forgeConsole.getRuntime();
 		String newVersion = runtime.getVersion();
@@ -63,12 +59,13 @@ public enum ForgeConsoleManager {
 		}
 		return consoles.size();
 	}
-	
+
 	private boolean isNewer(String first, String second) {
 		String[] firstValues = first.split("\\.|-");
 		String[] secondValues = second.split("\\.|-");
 		for (int i = 0; i < 2; i++) {
-			if (Integer.valueOf(firstValues[i]) < Integer.valueOf(secondValues[i])) {
+			if (Integer.valueOf(firstValues[i]) < Integer
+					.valueOf(secondValues[i])) {
 				return false;
 			}
 		}
