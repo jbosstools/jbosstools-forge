a281a8f82f33e0e3e53bfb21382f0f194f00fe40
Josef Kopriva <jkopriva@redhat.com>
2017-11-13 12:56:55 +0100

    JBIDE-23823 Move forge.reddeer plugin to jbosstools-forge (#176)
    Signed-off-by: Josef Kopriva <jkopriva@redhat.com>

---
+++ test-framework/org.jboss.tools.forge.reddeer/src/org/jboss/tools/forge/reddeer/ui/wizard/EntitiesFromTablesWizardFirstPage.java
@@ -0,0 +1,62 @@
+/******************************************************************************* 
+ * Copyright (c) 2017 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
+package org.jboss.tools.forge.reddeer.ui.wizard;
+
+import java.util.List;
+
+import org.eclipse.reddeer.core.reference.ReferencedComposite;
+import org.eclipse.reddeer.jface.wizard.WizardPage;
+import org.eclipse.reddeer.swt.impl.combo.DefaultCombo;
+import org.eclipse.reddeer.swt.impl.text.LabeledText;
+
+/**
+ * Reddeer implementation of forge Entities from tables wizard first page
+ * @author jrichter
+ *
+ */
+public class EntitiesFromTablesWizardFirstPage extends WizardPage {
+	
+	public EntitiesFromTablesWizardFirstPage(ReferencedComposite referencedComposite) {
+		super(referencedComposite);
+	}
+
+	/**
+	 * Fill in the Target package field with set value
+	 * @param pkg package name to be set
+	 */
+	public void setPackage(String pkg) {
+		new LabeledText("Target package:").setText(pkg);
+	}
+	
+	/**
+	 * Select a connection profile 
+	 * @param profileName name of the profile to be selected
+	 */
+	public void setConnectionProfile(String profileName) {
+		new DefaultCombo(0).setSelection(profileName);
+	}
+	
+	/**
+	 * Fill in the Connection Profile Password field
+	 * @param password
+	 */
+	public void setPassword(String password) {
+		new LabeledText("Connection Profile Password:").setText(password);
+	}
+	
+	/**
+	 * Get all saved profile names
+	 * @return List of all profile names
+	 */
+	public List<String> getAllProfiles() {
+		return new DefaultCombo(0).getItems();
+	}
+}
